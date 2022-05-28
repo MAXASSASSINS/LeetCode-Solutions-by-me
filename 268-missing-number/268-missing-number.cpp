71a87aa1e10eefa2771e9a7b4e8630c458f1747c
MAXASSASSINS <97503316+MAXASSASSINS@users.noreply.github.com>
2022-05-28 11:00:31 +0530

    Time: 27 ms (51.03%), Space: 17.9 MB (65.37%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -1,11 +1,21 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        for (int i = 0; i < nums.size(); ++i) {
-            if(nums[i] != i)
-                return i;
+        int i = 0;
+        while(i != nums.size()) {
+            if(nums[i] != i && nums[i] != nums.size()){
+                swap(nums[i], nums[nums[i]]);
+            }
+            else{
+                i++;
+            }
         }
+        for (int i = 0; i < nums.size(); ++i) {
+            if(nums[i] != i){
+                return i;
+            }
+        }
+
         return nums.size();
     }
 };
