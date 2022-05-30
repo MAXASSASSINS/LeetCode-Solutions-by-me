f4856a9c3e9c227768dbf9c9fc9a35d982e66728
MAXASSASSINS <97503316+MAXASSASSINS@users.noreply.github.com>
2022-05-30 18:50:16 +0530

    Time: 9 ms (15.36%), Space: 5.8 MB (96.19%) - LeetHub

--- 29-divide-two-integers/29-divide-two-integers.cpp
+++ 29-divide-two-integers/29-divide-two-integers.cpp
@@ -1,9 +1,31 @@
 class Solution {
 public:
     int divide(int dividend, int divisor) {
-        if(dividend == -2147483648 && divisor == -1){
-            return 2147483647;
+        
+        //Handling corner cases
+        if(dividend==INT_MIN && divisor==-1) return INT_MAX;
+        if(dividend==INT_MIN && divisor==1) return INT_MIN;
+
+        
+        //Converting divisors and dividend to their positive values
+        long int dd = abs(dividend), dv = abs(divisor);
+        
+        //Result Variables
+        int res=0;
+        while(dv<=dd) {
+            long int mul=dv, tmp=1;
+            while(mul<=dd-mul) {
+                mul+=mul;
+                tmp+=tmp;
+            }
+            res+=tmp;
+            dd-=mul;
         }
-        return dividend/divisor;
+        
+        //If either of dividend or divisor is negative our result will be negative
+        if((dividend<0&&divisor>0) || (dividend>0&&divisor<0)) return -res;
+        
+        return res;
+        
     }
 };
