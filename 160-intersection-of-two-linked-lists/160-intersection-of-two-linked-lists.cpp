10c531a1b10fab015efb5ec8ece037683f77314c
MAXASSASSINS <97503316+MAXASSASSINS@users.noreply.github.com>
2022-06-06 20:33:33 +0530

    Time: 83 ms (26.29%), Space: 14.5 MB (88.39%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -1,21 +1,35 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-
-        map<ListNode *, int> m;
-        while (headA != nullptr){
-            m[headA] = headA->val;
-            headA = headA->next;
+        int l1 = 0;
+        int l2 = 0;
+        ListNode *tempA = headA;
+        ListNode *tempB = headB;
+        while (tempA != nullptr){
+            l1++;
+            tempA = tempA->next;
         }
-
-        while (headB != nullptr){
-            int val = 0;
-            val = m[headB];
-            if(val != 0){
-                return headB;
-            }
-            headB = headB->next;
+        while (tempB != nullptr){
+            l2++;
+            tempB = tempB->next;
         }
-        return nullptr;
+        int move1 = max(0, l1 - l2);
+        int move2 = max(0, l2 - l1);
+
+        tempA = headA;
+        tempB = headB;
+        while (move1 > 0){
+            tempA = tempA->next;
+            move1--;
+        }
+        while (move2 > 0){
+            tempB = tempB->next;
+            move2--;
+        }
+        while(tempA != tempB){
+            tempA = tempA->next;
+            tempB = tempB->next;
+        }
+        return tempA;
     }
 };
