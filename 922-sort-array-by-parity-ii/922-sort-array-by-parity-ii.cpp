e253b5b1f624352eaa3c5c631d32da5a4042787f
MAXASSASSINS <97503316+MAXASSASSINS@users.noreply.github.com>
2022-06-28 14:47:22 +0530

    Time: 42 ms (24.42%), Space: 21.4 MB (67.47%) - LeetHub

--- 922-sort-array-by-parity-ii/922-sort-array-by-parity-ii.cpp
+++ 922-sort-array-by-parity-ii/922-sort-array-by-parity-ii.cpp
@@ -1,28 +1,15 @@
 class Solution {
 public:
     vector<int> sortArrayByParityII(vector<int>& nums) {
-//        sort(nums.begin(), nums.end());
         int n = nums.size();
-        int i = 0;
-        int j = 0;
-        while(i < nums.size() && j < nums.size()){
-            if(i % 2 == 1){
-                i++;
-            }
-            else if(j % 2 == 0){
-                j++;
-            }
-            else if(i % 2 == 0 && nums[i] % 2 == 0){
-                i++;
-            }
-            else if(j % 2 == 1 && nums[j] % 2 == 1){
-                j++;
-            }
-            else if(i % 2 == 0 && nums[i] % 2 == 1 && j % 2 == 1 &&  nums[j] % 2 == 0){
-                swap(nums[i], nums[j]);
-            }
+        int i = 0, j = n-1;
+        while(i<n) {
+            while(i<n && nums[i]%2==0) i+=2;
+            if(i==n) break;
+            while(j>=0 && nums[j]%2==1) j-=2;
+            //swap the outliers
+            swap(nums[i], nums[j]);
         }
-
         return nums;
     }
 };
