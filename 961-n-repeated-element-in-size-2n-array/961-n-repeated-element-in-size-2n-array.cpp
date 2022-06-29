9361d3f3b540a53dba042de2309be2626162d402
MAXASSASSINS <97503316+MAXASSASSINS@users.noreply.github.com>
2022-06-29 13:20:37 +0530

    Time: 41 ms (81.40%), Space: 24.7 MB (82.41%) - LeetHub

--- 961-n-repeated-element-in-size-2n-array/961-n-repeated-element-in-size-2n-array.cpp
+++ 961-n-repeated-element-in-size-2n-array/961-n-repeated-element-in-size-2n-array.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     int repeatedNTimes(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        for (int i = 0; i < nums.size() - 1; ++i) {
-            if(nums[i] == nums[i + 1]){
-                return nums[i];
-            }
-        }
+        for (int k = 1; k <= 3; ++k)
+            for (int i = 0; i < nums.size() - k; ++i)
+                if (nums[i] == nums[i+k])
+                    return nums[i];
+
         return 0;
     }
 };
