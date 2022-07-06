be26603d71776118dbcd0ccd9d8dcfbff9e60ea6
MAXASSASSINS <97503316+MAXASSASSINS@users.noreply.github.com>
2022-07-06 12:25:54 +0530

    Time: 23 ms (10.31%), Space: 18.7 MB (99.27%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.cpp
@@ -1,24 +1,10 @@
 class Solution {
 public:
-    void helper(int count, int &maxCount, TreeNode *root){
-        if(root == nullptr){
-            maxCount = max(count, maxCount);
-            cout<<maxCount<<endl;
-            return;
-        }
-        count++;
-        helper(count, maxCount, root->left);
-        helper(count, maxCount, root->right);
-
-    }
-
     int maxDepth(TreeNode* root) {
-        if (root == nullptr){
-            return 0;
-        }
-        int count = 0;
-        int maxCount = 1;
-        helper(count, maxCount, root);
-        return maxCount;
+        if(!root) return 0;
+        int maxLeft = maxDepth(root->left);
+        int maxRight = maxDepth(root->right);
+        return max(maxLeft, maxRight)+1;
     }
+
 };
