df0a630309b8acdc8696d3267628538f0d59e754
MAXASSASSINS <97503316+MAXASSASSINS@users.noreply.github.com>
2022-07-07 18:25:43 +0530

    Time: 3 ms (34.88%), Space: 6 MB (45.13%) - LeetHub

--- 168-excel-sheet-column-title/168-excel-sheet-column-title.cpp
+++ 168-excel-sheet-column-title/168-excel-sheet-column-title.cpp
@@ -1,23 +1,13 @@
 class Solution {
 public:
     string convertToTitle(int columnNumber) {
-        string ans;
-        while (columnNumber > 0) {
-            int val = columnNumber % 26;
-            if(val == 0){
-                ans.push_back('Z');
-                if (columnNumber == 26){
-                    break;
-                }
-                columnNumber /= 26;
-                columnNumber -= 1;
-                continue;
-            }
-            char c = char(val + 'A' - 1);
-            ans.push_back(c);
-            columnNumber /= 26;
+        string res = "";
+        while(columnNumber) {
+            columnNumber--;
+            res.push_back(columnNumber%26 + 'A');
+            columnNumber/=26;
         }
-        reverse(ans.begin(), ans.end());
-        return ans;
+        reverse(res.begin(), res.end());
+        return res;
     }
 };
