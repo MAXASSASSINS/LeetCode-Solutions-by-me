92c64eb5f42d3ba6a757caad7efeb9ff3ff4a5d6
MAXASSASSINS <97503316+MAXASSASSINS@users.noreply.github.com>
2022-07-08 10:58:39 +0530

    Time: 3 ms (62.07%), Space: 6.1 MB (55.70%) - LeetHub

---
+++ 202-happy-number/202-happy-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        unordered_map<int, vector<int>> ans;
+        while (n != 1){
+            int temp = 0;
+            while (n != 0){
+                temp += pow(n % 10, 2);
+                n /= 10;
+            }
+            if(temp == 1 || temp == 7){
+                return true;
+            }
+            else if(temp < 10 ){
+                return false;
+            }
+            n = temp;
+        }
+        return true;
+    }
+};
