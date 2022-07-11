2887a74386de704ffb2a12390a2bea7194f2791a
MAXASSASSINS <97503316+MAXASSASSINS@users.noreply.github.com>
2022-07-11 12:44:26 +0530

    Time: 6 ms (83.69%), Space: 8.4 MB (42.26%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.cpp
+++ 206-reverse-linked-list/206-reverse-linked-list.cpp
@@ -1,14 +1,24 @@
 class Solution {
 public:
-    ListNode* reverseList(ListNode* head) {
-       ListNode* next = nullptr;
-       ListNode *prev = nullptr;
-        while (head != nullptr){
-            next = head->next;
-            head->next = prev;
-            prev = head;
-            head = next;
+    ListNode *helper(ListNode *prev, ListNode *next, ListNode *head){
+        if (head == nullptr){
+            return prev;
         }
-        return prev;
+        next = head->next;
+        head->next = prev;
+        prev = head;
+        head = next;
+        return helper(prev, next, head);
+    }
+    ListNode* reverseList(ListNode* head) {
+        return helper(nullptr, nullptr, head);
+//       ListNode* next = nullptr;
+//       ListNode *prev = nullptr;
+//        while (head != nullptr){
+//            next = head->next;
+//            head->next = prev;
+//            prev = head;
+//            head = next;
+//        }
     }
 };
