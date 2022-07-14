6d0f1a96b7750aaace375a26f21996c0a424c265
MAXASSASSINS <97503316+MAXASSASSINS@users.noreply.github.com>
2022-07-14 11:13:06 +0530

    Time: 6 ms (77.33%), Space: 7.9 MB (72.43%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,21 +1,11 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int> ans;
-        for (int i = 0; i <= n; ++i) {
-            int count = 0;
-            int temp = i;
-            while(temp > 0){
-                if(temp % 2 == 0){
-                    temp /= 2;
-                }
-                else{
-                    temp -= 1;
-                    count++;
-                }
-            }
-            ans.push_back(count);
-        }
-        return ans;
+        vector<int> t(n+1);
+        t[0] = 0;
+        for(int i = 1; i<=n; ++i)
+            t[i] = t[i/2] + i%2;
+        
+        return t;
     }
 };
