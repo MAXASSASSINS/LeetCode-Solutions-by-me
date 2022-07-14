34bda861a05e5a62b060bd6c028911247bcafc35
MAXASSASSINS <97503316+MAXASSASSINS@users.noreply.github.com>
2022-07-14 13:21:40 +0530

    Time: 8 ms (24.86%), Space: 6.8 MB (25.69%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -1,13 +1,20 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
+        vector<int> vec(26,0);
         for (int i = 0; i < s.length(); ++i) {
-            if(s.at(i) != t.at(i)){
-                return t.at(i);
+            int val = s.at(i) - 'a';
+            vec[val]++;
+        }
+        for (int i = 0; i < t.length(); ++i) {
+            int val = t.at(i) - 'a';
+            vec[val]--;
+        }
+        for (int i = 0; i < 26; ++i) {
+            if(vec.at(i) == -1){
+                return char(i + 'a');
             }
         }
-        return t.at(t.length() - 1);
+        return '.';
     }
 };
