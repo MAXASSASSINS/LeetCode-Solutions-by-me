65afaabbf130077073524b054f41e8d646c649ff
MAXASSASSINS <97503316+MAXASSASSINS@users.noreply.github.com>
2022-07-17 13:15:17 +0530

    Time: 2 ms (62.64%), Space: 5.9 MB (78.84%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -4,6 +4,13 @@ public:
         if(n <= 1){
             return n;
         }
-        return fib(n - 1) + fib(n - 2);
+        int last = 1; int secondlast = 0;
+        int sum = 1;
+        for (int i = 2; i <= n; ++i) {
+            sum = last + secondlast; 
+            secondlast = last;
+            last = sum;
+        }
+        return sum;
     }
 };
