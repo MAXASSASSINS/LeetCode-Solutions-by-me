f00e99638eb867e9ff2e10d88c8f780dc6f0cec7
MAXASSASSINS <97503316+MAXASSASSINS@users.noreply.github.com>
2022-07-19 08:51:40 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (80.01%) - LeetHub

--- 551-student-attendance-record-i/551-student-attendance-record-i.cpp
+++ 551-student-attendance-record-i/551-student-attendance-record-i.cpp
@@ -1,22 +1,6 @@
 class Solution {
 public:
     bool checkRecord(string s) {
-        int n = s.length();
-        bool late3ConsecutiveDays = false;
-        int countAbsent = 0;
-        for (int i = 0; i < n; ++i) {
-            if(s.at(i) == 'A'){
-                countAbsent++;
-            }
-        }
-        for (int i = 0; i < n - 2; ++i) {
-            if(s.at(i) == 'L'){
-                if(s.at(i + 1) == 'L' && s.at(i + 2) == 'L'){
-                    late3ConsecutiveDays = true;
-                    break;
-                }
-            }
-        }
-        return !late3ConsecutiveDays && countAbsent < 2;
+        return s.find("LLL") == s.npos && std::count(s.begin(), s.end(),'A') < 2;
     }
 };
