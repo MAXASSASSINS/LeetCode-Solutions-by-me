e339220c594bc3a6db57e1aa737f50662120eb21
MAXASSASSINS <97503316+MAXASSASSINS@users.noreply.github.com>
2022-07-19 10:31:14 +0530

    Time: 21 ms (64.27%), Space: 10.7 MB (87.85%) - LeetHub

---
+++ 559-maximum-depth-of-n-ary-tree/559-maximum-depth-of-n-ary-tree.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxDepth(Node* root) {
+        if (root == nullptr) {
+            return 0;
+        }
+        
+        int max_depth = 1;
+        for (int i = 0; i < root->children.size(); i++) {
+            max_depth = max(max_depth, 1 + maxDepth(root->children[i]));
+        }
+        return max_depth;
+    }
+};
