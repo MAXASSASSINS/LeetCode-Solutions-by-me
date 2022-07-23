0a6848d104ba826baf65a0ed4c337815d888a388
MAXASSASSINS <97503316+MAXASSASSINS@users.noreply.github.com>
2022-07-23 11:40:31 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (83.12%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -1,39 +1,26 @@
 class Solution {
 public:
     vector<string> dialPad = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-    void helper(string digits, vector<string> &ans, int digit, queue<string> q){
-        string temp = dialPad.at(digit);
-        for (int i = 0; i < temp.size(); ++i) {
-            if(q.empty()){
-                string s;
-                s += temp.at(i);
-               ans.push_back(s);
-            }
-            else{
-                for (int j = 0; j < q.size(); ++j) {
-                    string s = q.front();
-                    string s2 = s +  temp.at(i);
-                    ans.push_back(s2);
-                    q.pop();
-                    q.push(s);
-                }
-            }
+
+    void helper(string digits, string processed, vector<string> &ans){
+        if(digits.empty()){
+            ans.push_back(processed);
+            return;
+        }
+        int digit = digits.at(0) - '0';
+        string s = dialPad[digit];
+        for (int i = 0; i < s.length(); ++i) {
+            char c = s.at(i);
+            helper(digits.substr(1), processed + c, ans);
         }
     }
 
     vector<string> letterCombinations(string digits) {
         vector<string> ans;
-        int i = 0;
-        while(i < digits.length()){
-            queue<string> q;
-            int digit = digits.at(i) - '0';
-            for (int j = 0; j < ans.size(); ++j) {
-                q.push(ans.at(j));
-            }
-            ans.clear();
-            helper("", ans, digit, q);
-            i++;
+        if(digits.empty()){
+            return ans;
         }
+        helper(digits, "", ans);
         return ans;
     }
 };
