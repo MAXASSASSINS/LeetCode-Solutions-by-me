5e9946140b7b87a4fe2c6ca79f9b0b3ddca6f3d1
MAXASSASSINS <97503316+MAXASSASSINS@users.noreply.github.com>
2022-07-24 11:10:38 +0530

    Time: 3 ms (82.05%), Space: 7.9 MB (37.41%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,34 +1,25 @@
 class Solution {
 public:
-    void helper(const vector<int> &nums, vector<vector<int>> &ans, vector<int> temp){
-        if(temp.size() == nums.size()) {
-            ans.push_back(temp);
-            return;
+    vector<vector<int>> result;
+    
+    // Backtracking
+    void permutation(vector<int> &nums,int i,int n){
+        if(i==n){
+            result.push_back(nums);
+            return ;
         }
-        for (int i = 0; i < nums.size(); ++i) {
-            int n = nums.at(i);
-            if(temp.empty()){
-                temp.push_back(n);
-                helper(nums, ans, temp);
-                temp.pop_back();
-                continue;
-            }
-            auto it = find(temp.begin(), temp.end(), n);
-            if(it == temp.end()){
-                temp.push_back(n);
-            }
-            else{
-                continue;
-            }
-            helper(nums, ans, temp);
-            temp.pop_back();
+
+        for(int j=i;j<=n;j++){
+            swap( nums[i],nums[j]);
+            permutation(nums,i+1,n);
+            swap( nums[i],nums[j]);
         }
     }
-
+    
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>> ans;
-        vector<int> temp;
-        helper(nums, ans, temp);
-        return ans;
+        
+        permutation(nums,0,nums.size()-1);
+        
+        return result;
     }
 };
