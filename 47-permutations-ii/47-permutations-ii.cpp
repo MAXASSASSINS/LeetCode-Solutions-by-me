2541af0ab1342f4bf1987873504a29bc11f8035e
MAXASSASSINS <97503316+MAXASSASSINS@users.noreply.github.com>
2022-07-24 11:55:57 +0530

    Time: 9 ms (75.83%), Space: 9.4 MB (58.25%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -1,30 +1,20 @@
 class Solution {
 public:
-    void helper(const vector<int> &nums, vector<vector<int>> &ans, vector<bool> &isVisited, vector<int> &temp){
-        if(temp.size() == nums.size()){
-            auto it = std::find(ans.begin(), ans.end(),temp);
-            if(it == ans.end()){
-                ans.push_back(temp);
-            }
-            return;
-        }
-        for (int i = 0; i < nums.size(); ++i) {
-            if(!isVisited.at(i)){
-                temp.push_back(nums.at(i));
-                isVisited.at(i) = true;
-                helper(nums, ans, isVisited, temp);
-                temp.pop_back();
-                isVisited.at(i) = false;
-            }
-        }
-    }
-
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        vector<vector<int>> ans;
-        vector<bool> isVisited(nums.size(), false);
-        vector<int> temp;
-        helper(nums, ans, isVisited, temp);
-
-        return ans;
+        vector<vector<int>> output;
+        sort(begin(nums), end(nums));
+        generatePermutations(nums, output, 0);
+        return output;
+    }
+private:
+    void generatePermutations(vector<int> nums, vector<vector<int>>& output, int idx) {
+        if (idx == size(nums)) {
+            output.emplace_back(nums);
+        }
+        for (int i = idx; i < size(nums); ++i) {
+            if (i != idx && nums[i] == nums[idx]) continue;
+            swap(nums[i], nums[idx]);
+            generatePermutations(nums, output, idx + 1);
+        }
     }
 };
