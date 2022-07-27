84b63d631da98680a9dfcb611ae089f399d4a448
MAXASSASSINS <97503316+MAXASSASSINS@users.noreply.github.com>
2022-07-27 19:21:08 +0530

    Time: 3 ms (62.94%), Space: 8.3 MB (70.19%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
-    void sortColors(vector<int> &nums) {
-        vector<int> arr(3);
-        for (int i = 0; i < nums.size(); ++i) {
-            ++arr[nums[i]];
-        }
-        int temp = 0;
-        for (int i = 0; i < nums.size(); ++i) {
-            while (arr[temp] == 0 && temp < 3) {
-                temp++;
+     void sortColors(vector<int>& nums) {
+        
+        // initialize variables:
+        int low = 0, mid = 0, high = nums.size() - 1;
+        
+        // logic:
+        while(mid <= high)
+        {
+            switch(nums[mid])
+            {
+                case 0: swap(nums[low++], nums[mid++]); break;
+                
+                case 1: mid++; break;
+                
+                case 2: swap(nums[mid], nums[high--]); break;
             }
-            nums[i] = temp;
-            --arr[temp];
         }
     }
 };
