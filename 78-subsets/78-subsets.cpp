2b9383a955740029e5ee3c6002c1f8bed59c5708
MAXASSASSINS <97503316+MAXASSASSINS@users.noreply.github.com>
2022-07-27 22:29:47 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (70.41%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
-    void helper(const vector<int> &nums, vector<vector<int>> &ans, vector<int> temp, int index) {
-        if(index == nums.size()){
-            return;
-        }
-        helper(nums, ans, temp, index + 1);
-        temp.push_back(nums[index]);
-        helper(nums, ans, temp, index + 1);
-        ans.push_back(temp);
-    }
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> ans;
         vector<int> temp;
         ans.push_back(temp);
-        int index = 0;
-        helper(nums, ans, temp, index);
+        for (int i = 0; i < nums.size(); ++i) {
+            int n = ans.size();
+            for (int j = 0; j < n; ++j) {
+                vector<int> temp2 = ans[j];
+                temp2.push_back(nums[i]);
+                ans.push_back(temp2);
+            }
+        }
         return ans;
     }
 };
