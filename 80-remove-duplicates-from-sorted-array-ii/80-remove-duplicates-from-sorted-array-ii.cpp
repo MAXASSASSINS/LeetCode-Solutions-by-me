b71c839fc4cef045ed8bb8bfb4a2f33a428ecfeb
MAXASSASSINS <97503316+MAXASSASSINS@users.noreply.github.com>
2022-07-27 23:07:03 +0530

    Time: 14 ms (20.94%), Space: 10.9 MB (73.78%) - LeetHub

--- 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.cpp
+++ 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.cpp
@@ -1,41 +1,16 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int ans = -1;
-        int i = 1;
-        int count = 1;
-        int prevVal = nums[0];
-        while (i < nums.size()){
-            if(nums[i]  == prevVal){
-                count++;
-            }
-            else{
-                count = 1;
-            }
-            prevVal = nums[i];
-            if(count > 2){
-                nums[i] = INT_MAX;
-            }
-            i++;
+        int n =nums.size();
+        if(n < 3) return n;
+        
+        int indx = 2;
+        for(int i =2; i< nums.size(); i++){
+             if(nums[i] != nums[indx -2]){
+                 nums[indx] = nums[i];
+                 indx++;
+             }
         }
-        for (int j = 0; j < nums.size(); ++j) {
-            if(nums[j] == INT_MAX){
-                int prevJIndex = j;
-                while (j < nums.size() && nums[j] == INT_MAX){
-                    j++;
-                }
-                if(j == nums.size()){
-                    break;
-                }
-                swap(nums[j], nums[prevJIndex]);
-                j = prevJIndex;
-            }
-        }
-        for (int j = 0; j < nums.size(); ++j) {
-            if(nums[j] == INT_MAX){
-                return j;
-            }
-        }
-        return nums.size();
-    }
+        return indx;
+  }
 };
