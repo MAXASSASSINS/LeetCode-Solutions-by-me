a21df814af19b620cb2329d8a49295afce75925f
MAXASSASSINS <97503316+MAXASSASSINS@users.noreply.github.com>
2022-08-03 14:24:26 +0530

    Time: 0 ms (100.00%), Space: 6 MB (50.43%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.cpp
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -1,13 +1,22 @@
 class Solution {
 public:
     int maximum69Number (int num) {
-        string s = to_string(num);
-        for (int i = 0; i < s.length(); ++i) {
-            if(s.at(i) == '6'){
-                s.at(i) = '9';
+        vector<int> arr;
+        while (num != 0){
+            arr.push_back(num % 10);
+            num /= 10;
+        }
+        for (int i = arr.size() -1; i >= 0 ; --i) {
+            if(arr[i] == 6){
+                arr[i] = 9;
                 break;
             }
         }
-        return stoi(s);
+        int ans = 0;
+        for (int i = arr.size() - 1; i >= 0; --i) {
+            ans *= 10;
+            ans += arr[i];
+        }
+        return ans;
     }
 };
