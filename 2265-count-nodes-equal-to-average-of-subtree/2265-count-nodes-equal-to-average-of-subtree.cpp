5001002f6f5efb8f826151c1138de190f5caf2e0
MAXASSASSINS <97503316+MAXASSASSINS@users.noreply.github.com>
2022-08-05 19:24:26 +0530

    Time: 11 ms (74.43%), Space: 11.9 MB (66.54%) - LeetHub

---
+++ 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.cpp
@@ -0,0 +1,19 @@
+
+class Solution {
+public:
+    int count=0;
+pair<int,int> valueSum(TreeNode* root){
+	if(root==NULL) return {0,0};
+	auto left=valueSum(root->left);
+	auto right=valueSum(root->right);
+	int sum=(left.first+right.first+root->val);
+	int n=(left.second+right.second+1);        
+	count+=((sum/n)==root->val);
+	return {sum,n};
+}
+int averageOfSubtree(TreeNode* root) {
+	auto p1=valueSum(root);
+	return count;
+}
+
+};
