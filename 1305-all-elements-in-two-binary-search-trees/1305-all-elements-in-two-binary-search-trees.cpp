6a913ff09c115c50c7a693a53e851cc17094343d
MAXASSASSINS <97503316+MAXASSASSINS@users.noreply.github.com>
2022-08-07 11:17:29 +0530

    Time: 278 ms (25.64%), Space: 84.4 MB (91.17%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
@@ -1,41 +1,31 @@
 class Solution {
 public:
-    void helper(TreeNode *root, vector<int> &ans){
-        if(root == nullptr){
-            return;
-        }
-        helper(root->left, ans);
-        ans.push_back(root->val);
-        helper(root->right, ans);
-    }
-
     vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
-        vector<int> a;
-        vector<int> b;
-        vector<int> ans;
-        helper(root1, a);
-        helper(root2, b);
-        int i = 0;
-        int j = 0;
-        while (i < a.size() && j < b.size()){
-            if(a.at(i) <= b.at(j)){
-                ans.push_back(a.at(i));
-                i++;
+        stack<TreeNode *> st1, st2;
+        vector<int> res;
+        
+        while(root1 || root2 || !st1.empty() || !st2.empty()){
+            while(root1){
+                st1.push(root1);
+                root1 = root1->left;
+            }
+            while(root2){
+                st2.push(root2);
+                root2 = root2->left;
+            }
+            if(st2.empty() || (!st1.empty() && st1.top()->val <= st2.top()->val)){
+                root1 = st1.top();
+                st1.pop();
+                res.push_back(root1->val);
+                root1 = root1->right;
             }
             else{
-                ans.push_back(b.at(j));
-                j++;
+                root2 = st2.top();
+                st2.pop();
+                res.push_back(root2->val);
+                root2 = root2->right;
             }
         }
-        while (i < a.size()){
-            ans.push_back(a.at(i));
-            i++;
-        }
-        while (j < b.size()){
-            ans.push_back(b.at(j));
-            j++;
-        }
-        
-        return ans;
+        return res;
     }
 };
