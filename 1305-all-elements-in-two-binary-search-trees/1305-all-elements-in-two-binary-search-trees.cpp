1f484a75b0be31d6f2993fe74cda7e27f6f5e633
MAXASSASSINS <97503316+MAXASSASSINS@users.noreply.github.com>
2022-08-07 10:51:18 +0530

    Time: 300 ms (15.68%), Space: 86.8 MB (26.01%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
@@ -1,6 +1,5 @@
 class Solution {
 public:
-
     void helper(TreeNode *root, vector<int> &ans){
         if(root == nullptr){
             return;
@@ -8,15 +7,35 @@ public:
         helper(root->left, ans);
         ans.push_back(root->val);
         helper(root->right, ans);
-
-
     }
 
     vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
+        vector<int> a;
+        vector<int> b;
         vector<int> ans;
-        helper(root1, ans);
-        helper(root2, ans);
-        sort(ans.begin(), ans.end());
+        helper(root1, a);
+        helper(root2, b);
+        int i = 0;
+        int j = 0;
+        while (i < a.size() && j < b.size()){
+            if(a.at(i) <= b.at(j)){
+                ans.push_back(a.at(i));
+                i++;
+            }
+            else{
+                ans.push_back(b.at(j));
+                j++;
+            }
+        }
+        while (i < a.size()){
+            ans.push_back(a.at(i));
+            i++;
+        }
+        while (j < b.size()){
+            ans.push_back(b.at(j));
+            j++;
+        }
+        
         return ans;
     }
 };
