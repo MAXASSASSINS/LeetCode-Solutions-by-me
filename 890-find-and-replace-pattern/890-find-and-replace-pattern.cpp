b3f386e6d2fbfa44d5e5c2c45ab2e48ccda9bfc4
MAXASSASSINS <97503316+MAXASSASSINS@users.noreply.github.com>
2022-08-07 14:01:29 +0530

    Time: 4 ms (76.59%), Space: 8.4 MB (69.63%) - LeetHub

--- 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
-    vector<string> findAndReplacePattern(vector<string>& words, string p) {        
+    vector<string> findAndReplacePattern(vector<string>& words, string& pattern) {        
 	vector<string> ans;
+	string p = normalise(pattern);
 	for(auto& w : words) 
-		if(match(w, p)) ans.push_back(w);
+		if(p == normalise(w)) ans.push_back(w);
 	return ans;
 }
-bool match(string& w1, string& w2) {
-	unordered_map<char, char> w1_to_w2, w2_to_w1;
-	for(int i = 0; i < size(w1); i++) 
-		if( (w1_to_w2.count(w1[i]) && w1_to_w2[w1[i]] != w2[i]) || 
-			(w2_to_w1.count(w2[i]) && w2_to_w1[w2[i]] != w1[i]) ) 
-			return false; 
-		else
-			w1_to_w2[w1[i]] = w2[i],
-			w2_to_w1[w2[i]] = w1[i];
-	return true;
+string normalise(string w) {
+	unordered_map<char, char> mp;
+	char c = 'a';
+	for(auto ch : w) 
+		if(!mp.count(ch)) mp[ch] = c++;
+	for(auto& ch : w) ch = mp[ch];
+	return w;
 }
 };
