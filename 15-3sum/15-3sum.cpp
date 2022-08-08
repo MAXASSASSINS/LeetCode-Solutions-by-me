ab46cd66e88efe1baff74a7572bd172df30fb97f
MAXASSASSINS <97503316+MAXASSASSINS@users.noreply.github.com>
2022-08-08 09:26:56 +0530

    Time: 306 ms (21.26%), Space: 20.4 MB (52.46%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,48 +1,31 @@
-class Solution
-{
+class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int> &nums)
-    {
-
-        int n = nums.size();
-        vector<vector<int>> v;
-        
-        // Sorting
-        sort(nums.begin(), nums.end());
-        for (int k = 0; k < n - 2; k++)
-        {
-            // Skipping Duplicates
-            if(k!=0 && nums[k]==nums[k-1]){
-                continue;
-            }
-            
-            int i = k + 1, j = n - 1;
-            while (i < j && nums[j]>=0)
-            {
-                int temp = nums[k] + nums[i] + nums[j];
-                
-                if (temp < 0)
-                {
-                    i++;
-                }
-                else if (temp > 0)
-                {
-                    j--;
-                }
-                else
-                {
-                    v.push_back({nums[i++], nums[k], nums[j--]});
-                    
-                    // Skipping Duplicates
-                    while(i<j && nums[i]==nums[i-1]){
-                        i++;
-                    }
-                    while(i<j && nums[j]==nums[j+1]){
-                        j--;
-                    }
-                }
-            }
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin() , nums.end());     //Sorted Array
+        if(nums.size() < 3){                // Base Case 1
+            return {};
         }
-        return v;
+        if(nums[0] > 0){                    // Base Case 2
+            return {};
+        }
+        unordered_map<int , int> hashMap;
+        for(int i = 0 ; i < nums.size() ; ++i){     //Hashing of Indices
+            hashMap[nums[i]] = i;
+        }
+        vector<vector<int>> answer;
+        for(int i = 0 ; i < nums.size() - 2 ; ++i){     //Traversing the array to fix the number.
+            if(nums[i] > 0){     //If number fixed is +ve, stop there because we can't make it zero by searching after it.
+                break;
+            }
+            for(int j = i + 1 ; j < nums.size() - 1 ; ++j){     //Fixing another number after first number
+                int required = -1*(nums[i] + nums[j]);    //To make sum 0, we would require the -ve sum of both fixed numbers.
+                if(hashMap.count(required) && hashMap.find(required)->second > j){ //If it exists in hashmap and its last occurrence index > 2nd fixed index, we found our triplet.
+                    answer.push_back({nums[i] , nums[j] , required});
+                }
+                j = hashMap.find(nums[j])->second; //Update j to last occurence of 2nd fixed number to avoid duplicate triplets.
+            }
+            i = hashMap.find(nums[i])->second;     //Update i to last occurence of 1st fixed number to avoid duplicate triplets.
+        }
+        return answer;  //Return answer vector.
     }
 };
