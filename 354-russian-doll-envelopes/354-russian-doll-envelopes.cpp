59a5b781e2927a70dc0a8574bf9f1041644b6ddb
MAXASSASSINS <97503316+MAXASSASSINS@users.noreply.github.com>
2022-08-23 22:55:33 +0530

    Time: 728 ms (26.14%), Space: 77.5 MB (67.61%) - LeetHub

--- 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
+++ 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     static bool comp(vector<int> &a, vector<int> &b){
-        if(a[0] == b[0])
-            return a[1] > b[1];
+        if(a[0]==b[0]){
+            return a[1]>b[1];
+        }
         return a[0]<b[0];
     }
+        
     int maxEnvelopes(vector<vector<int>>& envelopes) {
         sort(envelopes.begin(), envelopes.end(), comp);
-        int n = envelopes.size();
-        vector<int> ans;
-        ans.push_back(envelopes[0][1]);
-        for (int i = 0; i < n; ++i) {
-            if(envelopes[i][1] > ans.back())
-                ans.push_back(envelopes[i][1]);
+        int i, j, n=envelopes.size();
+        vector<int>lis;
+        
+        for(i=0; i<n; i++){
+            auto it = lower_bound(lis.begin(), lis.end(), envelopes[i][1]);
+            if(it==lis.end()){lis.push_back(envelopes[i][1]);}
             else{
-                int index = lower_bound(ans.begin(), ans.end(), envelopes[i][1]) - ans.begin();
-                ans[index] = envelopes[i][1];
+                *it = envelopes[i][1];
             }
         }
-        return ans.size();
+        return lis.size();
     }
 };
