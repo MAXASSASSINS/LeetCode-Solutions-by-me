bfcfdf7f6d23258f49b4859918c3a7b3c29f221f
MAXASSASSINS <97503316+MAXASSASSINS@users.noreply.github.com>
2022-08-25 20:19:01 +0530

    Time: 90 ms (25.43%), Space: 8.3 MB (61.07%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -42,9 +42,29 @@ public:
         return dp[dice][target];
     }
 
+
+    int solveTab(int d, int f, int t){
+        vector<vector<int>>dp(d + 1, vector<int>(t + 1, 0));
+        dp[0][0] = 1;
+        for (int dice = 1; dice <= d ; ++dice) {
+            for (int target = 1; target <= t; ++target) {
+                int ans = 0;
+                for (int i = 1; i <= f; ++i) {
+                    if(target - i >= 0){
+                        ans = (ans + dp[dice - 1][target - i]) % modulo;
+                    }
+                }
+                dp[dice][target] = ans % modulo;
+            }
+        }
+        return dp[d][t];
+    }
+    
+    
     int numRollsToTarget(int n, int k, int target) {
 //        return solve(n, k , target);
-        vector<vector<int>>dp(n + 1, vector<int>(target + 1, -1)) ;
-        return solveMem(n, k, target, dp);
+//        vector<vector<int>>dp(n + 1, vector<int>(target + 1, -1)) ;
+//        return solveMem(n, k, target, dp);
+        return solveTab(n, k, target);
     }
 };
