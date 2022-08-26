38f7cc633b3dd077c83dd43e79d22ee4d7de5236
MAXASSASSINS <97503316+MAXASSASSINS@users.noreply.github.com>
2022-08-26 20:55:22 +0530

    Time: 493 ms (19.90%), Space: 120.4 MB (40.74%) - LeetHub

--- 801-minimum-swaps-to-make-sequences-increasing/801-minimum-swaps-to-make-sequences-increasing.cpp
+++ 801-minimum-swaps-to-make-sequences-increasing/801-minimum-swaps-to-make-sequences-increasing.cpp
@@ -83,6 +83,51 @@ public:
         }
         return dp[1][0];
     }
+
+    int solveSO(vector<int> &nums1, vector<int> &nums2){
+        int n = nums1.size();
+        vector<vector<int>> dp(n + 1, vector<int>(2, 0));
+
+        int swap = 0;
+        int noswap = 0;
+        int currswap = 0;
+        int currnoswap = 0;
+
+
+        for (int index = n - 1; index >= 1; --index) {
+            for (int swapped = 1; swapped >= 0 ; --swapped) {
+                int ans = INT_MAX;
+                int prev1 = nums1[index - 1];
+                int prev2 = nums2[index - 1];
+
+
+                if(swapped){
+                    int temp = prev1;
+                    prev1 = prev2;
+                    prev2 = temp;
+                }
+
+                //noswap
+                if(nums1[index] > prev1 && nums2[index] > prev2){
+                    ans = dp[index + 1][0];
+                }
+
+                //swap
+                if(nums1[index] > prev2 && nums2[index] > prev1){
+                    ans = min(ans, 1 + dp[index + 1][1]);
+                }
+
+                if (swapped)
+                    currswap = ans;
+                else
+                    currnoswap = ans;
+                dp[index][swapped] = ans;
+            }
+            swap = currswap;
+            noswap = currnoswap;
+        }
+        return min(swap, noswap);
+    }
     
     int minSwap(vector<int>& nums1, vector<int>& nums2) {
         nums1.insert(nums1.begin(), -1);
@@ -92,6 +137,7 @@ public:
         int n = nums1.size();
 //        vector<vector<int>> dp(n + 1, vector<int>(2,-1));
 //        return solveMem(nums1, nums2, 1, false, dp);
-        return solveTab(nums1, nums2);
+//        return solveTab(nums1, nums2);
+        return solveSO(nums1, nums2);
     }
 };
