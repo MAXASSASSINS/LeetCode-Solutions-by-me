1c876040f392a27a875664ec659cdbdb54e5e4d3
MAXASSASSINS <97503316+MAXASSASSINS@users.noreply.github.com>
2022-08-26 20:40:44 +0530

    Time: 443 ms (28.47%), Space: 120.3 MB (45.87%) - LeetHub

--- 801-minimum-swaps-to-make-sequences-increasing/801-minimum-swaps-to-make-sequences-increasing.cpp
+++ 801-minimum-swaps-to-make-sequences-increasing/801-minimum-swaps-to-make-sequences-increasing.cpp
@@ -54,13 +54,44 @@ public:
         return dp[index][swapped] = ans;
     }
 
+    int solveTab(vector<int> &nums1, vector<int> &nums2){
+        int n = nums1.size();
+        vector<vector<int>> dp(n + 1, vector<int>(2, 0));
+
+        for (int index = n - 1; index >= 1; --index) {
+            for (int swapped = 1; swapped >= 0 ; --swapped) {
+                int ans = INT_MAX;
+                int prev1 = nums1[index - 1];
+                int prev2 = nums2[index - 1];
+
+
+                if(swapped){
+                    swap(prev1, prev2);
+                }
+
+                //noswap
+                if(nums1[index] > prev1 && nums2[index] > prev2){
+                    ans = dp[index + 1][0];
+                }
+
+                //swap
+                if(nums1[index] > prev2 && nums2[index] > prev1){
+                    ans = min(ans, 1 + dp[index + 1][1]);
+                }
+                dp[index][swapped] = ans;
+            }
+        }
+        return dp[1][0];
+    }
+    
     int minSwap(vector<int>& nums1, vector<int>& nums2) {
         nums1.insert(nums1.begin(), -1);
         nums2.insert(nums2.begin(), -1);
         bool swapped = false;
 //        return solve(nums1, nums2, 0, swapped);
         int n = nums1.size();
-        vector<vector<int>> dp(n + 1, vector<int>(2,-1));
-        return solveMem(nums1, nums2, 1, false, dp);
+//        vector<vector<int>> dp(n + 1, vector<int>(2,-1));
+//        return solveMem(nums1, nums2, 1, false, dp);
+        return solveTab(nums1, nums2);
     }
 };
