8d765310c5d3a9ab5e397dd268507841fd8e523a
MAXASSASSINS <97503316+MAXASSASSINS@users.noreply.github.com>
2022-08-27 11:01:00 +0530

    Time: 36 ms (51.27%), Space: 13.1 MB (22.77%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -34,12 +34,30 @@ public:
         }
         return dp[i][j] = max(include, exclude);
     }
-    
-    
-    
+
+    int solveTab(string &text1, string &text2){
+        vector<vector<int>> dp(text1.length() + 1, vector<int>(text2.length() + 1, 0));
+        for (int i = text1.length() - 1; i >= 0 ; --i) {
+            for (int j = text2.length() - 1; j >= 0; --j) {
+                int include = 0;
+                int exclude = 0;
+                if (text1[i] == text2[j]){
+                    include = 1 + dp[i + 1][j + 1];
+                }
+                else{
+                    exclude = max(dp[i + 1][j], dp[i][j + 1]);
+                }
+                dp[i][j] = max(include, exclude);
+            }
+        }
+        return dp[0][0];
+    }
+
+
     int longestCommonSubsequence(string text1, string text2) {
 //        return solve(text1, text2, 0, 0);
-        vector<vector<int>> dp(text1.size(), vector<int>(text2.size(), -1));
-        return solveMem(text1, text2, 0, 0, dp);
+//        vector<vector<int>> dp(text1.size(), vector<int>(text2.size(), -1));
+//        return solveMem(text1, text2, 0, 0, dp);
+        return solveTab(text1, text2);
     }
 };
