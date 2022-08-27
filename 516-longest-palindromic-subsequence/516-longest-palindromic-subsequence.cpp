72927e4eae20974f429fd5310becc80df16530d5
MAXASSASSINS <97503316+MAXASSASSINS@users.noreply.github.com>
2022-08-27 11:22:06 +0530

    Time: 403 ms (11.20%), Space: 73.1 MB (27.72%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-        int solveMem(string &text1, string &text2, int i, int j, vector<vector<int>> &dp){
+    int solveMem(string &text1, string &text2, int i, int j, vector<vector<int>> &dp){
         if (i == text1.length() || j == text2.length()){
             return 0;
         }
@@ -19,11 +19,29 @@ public:
         return dp[i][j] = max(include, exclude);
     }
 
+    int solveTab(string &text1, string &text2) {
+        vector<vector<int>> dp(text1.length() + 1, vector<int>(text2.length() + 1, 0));
+        for (int i = text1.length() - 1; i >= 0; --i) {
+            for (int j = text2.length() - 1; j >= 0; --j) {
+                int include = 0;
+                int exclude = 0;
+                if (text1[i] == text2[j]) {
+                    include = 1 + dp[i + 1][j + 1];
+                } else {
+                    exclude = max(dp[i + 1][j], dp[i][j + 1]);
+                }
+                dp[i][j] = max(include, exclude);
+            }
+        }
+
+        return dp[0][0];
+    }
+    
     int longestCommonSubsequence(string text1, string text2) {
 //        return solve(text1, text2, 0, 0);
-        vector<vector<int>> dp(text1.size(), vector<int>(text2.size(), -1));
-        return solveMem(text1, text2, 0, 0, dp);
-//        return solveTab(text1, text2);
+        // vector<vector<int>> dp(text1.size(), vector<int>(text2.size(), -1));
+//        return solveMem(text1, text2, 0, 0, dp);
+        return solveTab(text1, text2);
 //        return solveSO(text1, text2);
     }
 
