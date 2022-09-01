01aebb66fb4ceeb0c0bd5ad00a363f6e8079ef0d
MAXASSASSINS <97503316+MAXASSASSINS@users.noreply.github.com>
2022-09-01 19:28:54 +0530

    Time: 21 ms (48.21%), Space: 12.8 MB (41.64%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,99 +1,54 @@
 class Solution {
 public:
-    int solve(vector<int> &prices, int buy, int index, int limit){
-        if (index == prices.size() || limit == 0){
-            return 0;
-        }
-        int profit = 0;
-        if (buy){
-            int buyKaro = -prices[index] + solve(prices, 0, index + 1, limit);
-            int skipKaro = solve(prices, 1, index + 1, limit);
-            profit = max(buyKaro, skipKaro);
-        }
-        else{
-            int sellKaro = prices[index] + solve(prices, 1, index + 1, limit - 1);
-            int skipKaro = solve(prices, 0, index + 1, limit);
-            profit = max(sellKaro, skipKaro);
-        }
-        return profit;
-    }
-
-    int solveMem(vector<int> &prices, int buy, int index, vector<vector<vector<int>>> &dp, int limit){
+    int solve(int k, vector<int> &prices, int operationNo, int index){
         if (index == prices.size()){
             return 0;
         }
-        if (dp[index][buy][limit] != -1)
-            return dp[index][buy][limit];
-
-        int profit = 0;
-        if (buy){
-            int buyKaro = -prices[index] + solveMem(prices, 0, index + 1, dp, limit);
-            int skipKaro = solveMem(prices, 1, index + 1, dp, limit);
-            profit = max(buyKaro, skipKaro);
+        if(operationNo == 2 * k){
+            return 0;
+        }
+        int ans = 0;
+        if (operationNo % 2 == 0){
+            int buyKaro = -prices[index]  + solve(k, prices, operationNo + 1, index + 1);
+            int skipKaro = solve(k, prices, operationNo, index + 1);
+            ans = max(buyKaro, skipKaro);
         }
         else{
-            int sellKaro = prices[index] + solveMem(prices, 1, index + 1, dp, limit - 1);
-            int skipKaro = solveMem(prices, 0, index + 1, dp, limit);
-            profit = max(sellKaro, skipKaro);
+            int sellKaro = prices[index]  + solve(k, prices, operationNo + 1, index + 1);
+            int skipKaro = solve(k, prices, operationNo, index + 1);
+            ans = max(sellKaro, skipKaro);
         }
-        return dp[index][buy][limit] = profit;
+        return ans;
     }
 
-    int solveTab(int k, vector<int> &prices){
-        int n = prices.size();
-        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>> (2, vector<int> (k + 1, 0)));
-        for (int index = n - 1; index >= 0 ; --index) {
-            for (int buy = 0; buy <= 1; ++buy) {
-                for (int limit = 1; limit <= k; ++limit) {
-                    int profit = 0;
-                    if (buy) {
-                        int buyKaro = -prices[index] + dp[index + 1][0][limit];
-                        int skipKaro = dp[index + 1][1][limit];
-                        profit = max(buyKaro, skipKaro);
-                    } else {
-                        int sellKaro = prices[index] + dp[index + 1][1][limit - 1];
-                        int skipKaro = dp[index + 1][0][limit];
-                        profit = max(sellKaro, skipKaro);
-                    }
-                    dp[index][buy][limit] = profit;
-                }
-            }
+    int solveMem(int k, vector<int> &prices, int operationNo, int index, vector<vector<int>> &dp){
+        if (index == prices.size()){
+            return 0;
         }
-        return dp[0][1][k];
-    }
-
-    int solveSO(int k, vector<int> &prices){
-        int n = prices.size();
-        vector<vector<int>> curr(2, vector<int> (k + 1, 0));
-        vector<vector<int>> next(2, vector<int> (k + 1, 0));
-
-        for (int index = n - 1; index >= 0 ; --index) {
-            for (int buy = 0; buy <= 1; ++buy) {
-                for (int limit = 1; limit <= k; ++limit) {
-                    int profit = 0;
-                    if (buy) {
-                        int buyKaro = -prices[index] + next[0][limit];
-                        int skipKaro = next[1][limit];
-                        profit = max(buyKaro, skipKaro);
-                    } else {
-                        int sellKaro = prices[index] + next[1][limit - 1];
-                        int skipKaro = next[0][limit];
-                        profit = max(sellKaro, skipKaro);
-                    }
-                    curr[buy][limit] = profit;
-                }
-            }
-            next = curr;
+        if(operationNo == 2 * k){
+            return 0;
         }
-        return next[1][k];
+        if (dp[index][operationNo] != -1){
+            return dp[index][operationNo];
+        }
+        int ans = 0;
+        if (operationNo % 2 == 0){
+            int buyKaro = -prices[index]  + solveMem(k, prices, operationNo + 1, index + 1, dp);
+            int skipKaro = solveMem(k, prices, operationNo, index + 1, dp);
+            ans = max(buyKaro, skipKaro);
+        }
+        else{
+            int sellKaro = prices[index]  + solveMem(k, prices, operationNo + 1, index + 1, dp);
+            int skipKaro = solveMem(k, prices, operationNo, index + 1, dp);
+            ans = max(sellKaro, skipKaro);
+        }
+        return dp[index][operationNo] = ans;
     }
 
     int maxProfit(int k, vector<int>& prices) {
+//        return solve(k, prices, 0, 0);
         int n = prices.size();
-//        return solve(prices, 1, 0, 2);
-        vector<vector<vector<int>>> dp(n, vector<vector<int>> (2, vector<int> (k + 1, -1)));
-//        return solveMem(prices, 1,   0, dp, k);
-        return solveTab(k, prices);
-//        return solveSO(k, prices);
+        vector<vector<int>> dp(n + 1, vector<int> (2 * k, -1));
+    return solveMem(k, prices, 0, 0, dp);
     }
 };
