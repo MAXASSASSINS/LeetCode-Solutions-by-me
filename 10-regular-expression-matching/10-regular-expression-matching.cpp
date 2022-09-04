61f4edf32e253e657e3c719995f35d0d89be66e3
MAXASSASSINS <97503316+MAXASSASSINS@users.noreply.github.com>
2022-09-04 13:29:59 +0530

    Time: 6 ms (72.59%), Space: 7.4 MB (32.20%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.cpp
+++ 10-regular-expression-matching/10-regular-expression-matching.cpp
@@ -22,7 +22,33 @@ public:
         }
     }
 
+    bool solveMem(string &s, string &p, int i, int j, vector<vector<int>> &dp){
+        if(i >= s.length() && j >= p.length()){
+            return true;
+        }
+        if(j >= p.length() && i < s.length()){
+            return false;
+        }
+
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        bool match = i < s.length() && (s[i] == p[j] || p[j] == '.');
+
+        if(j + 1 < p.length() && p[j + 1] == '*'){
+            return dp[i][j] = (match && solveMem(s, p, i + 1, j, dp)) || solveMem(s, p, i, j + 2, dp);
+        }
+        else if(match){
+            return dp[i][j] = solveMem(s, p , i + 1, j + 1, dp);
+        }
+        else{
+            return dp[i][j] = false;
+        }
+    }
+
     bool isMatch(string s, string p) {
-        return solve(s, p, 0, 0);
+//        return solve(s, p, 0, 0);
+        vector<vector<int>> dp(s.length() + 1, vector<int> (p.length() + 1, - 1));
+        return solveMem(s, p, 0, 0, dp);
     }
 };
