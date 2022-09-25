243dfa916f864801d4bab0afb72325c263dde825
MAXASSASSINS <97503316+MAXASSASSINS@users.noreply.github.com>
2022-09-25 13:27:02 +0530

    Time: 355 ms (6.86%), Space: 45.6 MB (68.22%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -1,11 +1,24 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-	priority_queue<int,vector<int>,greater<int>> pq;
-	for(int i=0;i<nums.size();++i){
-		pq.push(nums[i]);
-		if(pq.size()>k) pq.pop();
+	buildheap(nums);//maxheap
+	for(int i=1;i<k;++i){
+		swap(nums[0],nums.back());
+		nums.pop_back();
+		heapify(nums,0);
 	}
-	return pq.top();            
+	return nums[0];
 }
+void heapify(vector<int>& nums,int i){
+	int leftIdx = (i<<1)+1, rightIdx = (i<<1)+2, largestIdx=i;
+	if(leftIdx<nums.size() && nums[leftIdx]>nums[largestIdx]) largestIdx = leftIdx;
+	if(rightIdx<nums.size() && nums[rightIdx]>nums[largestIdx]) largestIdx = rightIdx;
+	if(largestIdx!=i)
+		swap(nums[i], nums[largestIdx]), heapify(nums,largestIdx);
+}
+void buildheap(vector<int>& nums){
+	//lastnodeindex = n-1, so lastParentIdx= (n-1-1)/2 = n/2-1
+	for(int i=(nums.size()>>1)-1;i>=0;--i)  heapify(nums,i);
+}
+
 };
