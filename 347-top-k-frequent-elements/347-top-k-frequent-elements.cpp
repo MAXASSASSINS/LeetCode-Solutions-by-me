6811125bc8a2aed67ebe3959be5719de0dd931a2
MAXASSASSINS <97503316+MAXASSASSINS@users.noreply.github.com>
2022-09-27 20:01:09 +0530

    Time: 35 ms (27.92%), Space: 15.2 MB (14.37%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -4,14 +4,16 @@ public:
         unordered_map<int, int> freq;
         for (auto num : nums) freq[num]++;
         
-        priority_queue<pair<int, int>> pq;
-        for (auto [a, b] : freq) pq.push({b, a});
+        vector<vector<int>> buckets(nums.size()+1);
+        for (auto [a, b] : freq)
+            buckets[b].push_back(a);
         
         vector<int> res;
-        while (k) {
-            res.push_back(pq.top().second);
-            pq.pop();
-            k--;
+        for (int i = nums.size(); k; i--) {
+            for (auto a : buckets[i]) {
+                res.push_back(a);
+                k--;
+            }
         }
         
         return res;
