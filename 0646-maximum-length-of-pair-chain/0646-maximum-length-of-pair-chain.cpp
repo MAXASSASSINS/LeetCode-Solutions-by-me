fdeeb359214bf4a9b63daedc1e89292d5340163f
MAXASSASSINS <97503316+MAXASSASSINS@users.noreply.github.com>
2023-01-20 20:31:12 +0530

    Time: 749 ms (8.28%), Space: 159.1 MB (5.67%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
@@ -1,18 +1,46 @@
 class Solution {
 public:
-	int f(int i,int prev,vector<vector<int>>& pairs,int n,vector<vector<int>>& dp){
-		if(i==n) return 0;
-		if(dp[i][prev+1]!=-1) return dp[i][prev+1];
-		int pick=INT_MIN;
-		if(prev==-1 || pairs[prev][1]<pairs[i][0])pick=1+f(i+1,i,pairs,n,dp);
-		int notpick=f(i+1,prev,pairs,n,dp);
-		return dp[i][prev+1]=max(pick,notpick);
-	}
-
-	int findLongestChain(vector<vector<int>>& pairs) {
-		int n=pairs.size();
-		sort(pairs.begin(),pairs.end());
-		vector<vector<int>> dp(n,vector<int>(n+1,-1));
-		return f(0,-1,pairs,n,dp);
-	}
+    int solve(vector<vector<int>> &pairs, int index, int prev){
+        if(index >= pairs.size()) return 0;
+        
+        // exlucde 
+        int excl = solve(pairs, index + 1, prev);
+        
+        // include
+        int incl = 0;
+        if(prev == -1 || pairs[index][0] > pairs[prev][1]){
+            incl = 1 + solve(pairs, index + 1, index);            
+        }
+        
+        return max(incl, excl);
+        
+    }
+    
+    int solveMem(vector<vector<int>> &pairs, int index, int prev, vector<vector<int>> &dp){
+        if(index >= pairs.size()) return 0;
+        
+        if(dp[index][prev + 1] != -1){
+            return dp[index][prev + 1];
+        }
+        
+        // exlucde 
+        int excl = solveMem(pairs, index + 1, prev, dp);
+        
+        // include
+        int incl = 0;
+        if(prev == -1 || pairs[index][0] > pairs[prev][1]){
+            incl = 1 + solveMem(pairs, index + 1, index, dp);       
+        }
+        return dp[index][prev + 1] = max(incl, excl);
+        
+    }
+    
+    
+    int findLongestChain(vector<vector<int>>& pairs) {
+        int n = pairs.size();
+        vector<vector<int>> dp(n, vector<int>(n + 1, -1));
+        sort(pairs.begin(), pairs.end());
+        // return solve(pairs, 0, -1);
+        return solveMem(pairs, 0, -1, dp);
+    }
 };
