998d76eae2c4cc8e43fbf2434209e8daba03325b
MAXASSASSINS <97503316+MAXASSASSINS@users.noreply.github.com>
2023-01-21 08:39:15 +0530

    Time: 7 ms (22.07%), Space: 11.2 MB (5.01%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    bool check(const string &s){
+        int st = 0;
+        int en = 0;
+        vector<string> temp;
+        int len = s.length();
+        while(en < len){
+            while(en < len && s[en] != '.'){
+                en++;
+            }
+            string t = s.substr(st, en - st);
+            en++;
+            st = en;
+            
+            // checking string
+            if(t[0] == '0' && t.length() > 1) return false;
+            if(t.length() > 3 || t.length() == 0) return false;
+            if(stoi(t) > 255) return false;
+        }
+        
+        return true;
+    }
+    void solve(string s, vector<string> &ans, int index, int count){
+        if(count == 3){
+            if(check(s)) ans.push_back(s);
+            return;
+        }
+        if(index >= s.length()){
+            return;
+        }
+
+        // don't put dot        
+        solve(s, ans, index + 1, count);
+
+        
+        // put dot
+        s.insert(index, 1, '.');
+        solve(s, ans, index + 1, count + 1);
+        s.erase(index);
+        
+        
+        
+    }
+    vector<string> restoreIpAddresses(string s) {
+        vector<string> ans;
+        if(s.length() < 4 && s.length() > 12) return ans;
+        solve(s, ans, 0, 0);
+        return ans;
+    }
+};
