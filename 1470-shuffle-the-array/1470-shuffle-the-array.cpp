1fa6a210305d31db74fe85675eee4c78c30b39fc
MAXASSASSINS <97503316+MAXASSASSINS@users.noreply.github.com>
2023-02-06 12:00:25 +0530

    Time: 8 ms (36.69%), Space: 9.7 MB (61.93%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -1,12 +1,24 @@
 class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
-        vector<int> ans;
-        for(int i = 0; i < n; i++){
-            ans.push_back(nums[i]);
-            ans.push_back(nums[i + n]);
+        // Store each y(i) with respective x(i).
+        for (int i = n; i < 2 * n; ++i) {
+            int secondNum = nums[i] << 10;
+            nums[i - n] |= secondNum;
         }
-        
-        return ans;
+
+        // '0000000000 1111111111' in decimal.
+        int allOnes = pow(2, 10) - 1;
+
+        // We will start putting all numbers from the end, 
+        // as they are empty places.
+        for (int i = n - 1; i >= 0; --i) {
+            // Fetch both the numbers from the current index.
+            int secondNum = nums[i] >> 10;
+            int firstNum = nums[i] & allOnes;
+            nums[2 * i + 1] = secondNum;
+            nums[2 * i] = firstNum;
+        }
+        return nums;
     }
 };
