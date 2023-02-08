c7e78ca16c6720dbcbc9cd0261f98b13b8e73e6e
MAXASSASSINS <97503316+MAXASSASSINS@users.noreply.github.com>
2023-02-08 22:49:25 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (12.79%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> ans = {{1}};
+
+        if(numRows == 1){
+            return ans;
+        }
+        for(int i = 1; i < numRows; i++){
+            vector<int> temp;
+            temp.push_back(1);
+            for(int j = 0; j < i - 1; j++){
+                temp.push_back(ans[i - 1][j] + ans[i - 1][j + 1]);
+            }
+            temp.push_back(1);
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
