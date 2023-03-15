08c5f9ce810deabf0efb538294c6f72358b93194
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2023-03-15 21:24:09 +0530

    Time: 15 ms (44.44%), Space: 10.5 MB (81.25%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
@@ -1,19 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
         if(!head || !head->next || !head->next->next) return head;
+        ListNode* oddHead = head;
+        ListNode* evenHead = head->next;
+        ListNode* odd = oddHead;
+        ListNode* even = evenHead;
         
-        ListNode *odd = head;
-        ListNode *even = head->next;
-        ListNode *even_start = head->next;
-        
-        while(odd->next && even->next){
-            odd->next = even->next; //Connect all odds
-            even->next = odd->next->next;  //Connect all evens
-            odd = odd->next;
-            even = even->next;
+        bool flag = false;
+        while(odd && even){
+            if(!flag){
+                flag = true;
+                odd->next = even->next;
+                odd = even->next;
+            }
+            else{
+                flag = false;
+                even->next = odd->next;
+                even = odd->next;
+            }
         }
-        odd->next = even_start;   //Place the first even node after the last odd node.
-        return head; 
+        
+        if(!odd){
+            odd = oddHead;
+            while(odd->next != nullptr){
+                odd = odd->next;
+            }
+        }
+        
+        odd->next = evenHead;
+        
+        return head;
+        
+        
+        
     }
 };
