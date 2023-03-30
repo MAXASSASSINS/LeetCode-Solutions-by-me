2cc9f1a615a5bce16f48b2c59d7b8d9a82358a42
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2023-03-30 13:18:07 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (62.15%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void solve(int k, int n, int num,  vector<vector<int>> &ans, vector<int> &temp){
+        if(num > 9){
+            if(temp.size() == k && n == 0){
+                ans.push_back(temp);
+            }
+            return;
+        }
+        
+        // not take current number
+        solve(k , n, num + 1, ans, temp);
+        
+        // take current number
+        temp.push_back(num);
+        solve(k , n - num, num + 1, ans, temp);
+        temp.pop_back();
+        
+        
+    }
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<vector<int>> ans;
+        vector<int> temp;
+        solve(k, n, 1, ans, temp);
+        return ans;
+    }
+};
