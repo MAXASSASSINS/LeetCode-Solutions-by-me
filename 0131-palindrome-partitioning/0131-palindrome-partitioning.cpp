0165449018b27bd15e7bf687bf5ea792e8ecb828
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2023-04-29 13:41:30 +0530

    Time: 139 ms (48.83%), Space: 57.4 MB (69.56%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,23 +1,33 @@
 class Solution {
 public:
-    vector<vector<string>> partition(string s) {
-        int len = s.length();
-        vector<vector<bool>> dp (len, vector <bool> (len, false));
-        vector<vector<string>> result;
-        vector<string> currentList;
-        dfs(result, s, 0, currentList, dp);
-        return result;
-    }
-
-    void dfs(vector<vector<string>> &result, string &s, int start, vector<string> &currentList, vector<vector<bool>> &dp) {
-        if (start >= s.length()) result.push_back(currentList);
-        for (int end = start; end < s.length(); end++) {
-            if (s[start] == s[end] && (end - start <= 2 || dp[start + 1][end - 1])) {
-                dp[start][end] = true;
-                currentList.push_back(s.substr(start, end - start + 1));
-                dfs(result, s, end + 1, currentList, dp);
-                currentList.pop_back();
-            }
+    bool isPalindrome(string s, int st, int en){
+        while(st <= en){
+            if(s[st] != s[en]) return false;
+            st++;
+            en--;
         }
+        return true;
+    }
+    void solve(string s, vector<vector<string>> &ans,vector<string> &temp, int index){
+        if(index == s.length()){
+            ans.push_back(temp);
+            return;
+        }
+        for(int i = index; i < s.length(); i++){
+            // partitioning
+            if(isPalindrome(s, index, i)){
+                temp.push_back(s.substr(index, i - index + 1));
+                solve(s, ans, temp, i + 1);
+                temp.pop_back();
+            }
+            
+        }
+        
+    }
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> ans;
+        vector<string> temp;
+        solve(s, ans, temp, 0); 
+        return ans;
     }
 };
