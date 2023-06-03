a3e81fb0e16f54c0664c8a5796ad5bf1f83a4a56
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2023-06-03 17:30:57 +0530

    Time: 40 ms (57.76%), Space: 24.3 MB (31.83%) - LeetHub

---
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        stack<int> st;
+        
+        int n = nums.size();
+        vector<int> ans(2*n);
+        
+        for(int i = 2*n - 1; i >= 0; i--){
+            int num = nums[i % n];
+            while(!st.empty() && st.top() <= num){
+                st.pop();
+            }
+            
+            ans[i] = st.empty() ? -1 : st.top();
+            st.push(num);
+        }
+        
+        ans.resize(n);
+        
+        return ans;
+        
+        
+    }
+};
