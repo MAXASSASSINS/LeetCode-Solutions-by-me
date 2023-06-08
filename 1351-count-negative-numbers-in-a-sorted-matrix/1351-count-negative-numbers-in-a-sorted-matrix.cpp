89be70b7ee8ef9d3e34b5aba149aaed3f81317ec
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2023-06-08 12:46:53 +0530

    Time: 15 ms (82.04%), Space: 10.4 MB (31.99%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.cpp
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.cpp
@@ -1,29 +1,19 @@
 class Solution {
 public:
-    int binary(vector<int> &vec){
-        int h = vec.size() - 1;
-        int l = 0;
-        
-        int idx = -1;
-        
-        while(l <= h){
-            int mid = l + (h - l)/2;
-            if(vec[mid] < 0){
-                idx = mid;
-                h = mid - 1;
-            }
-            else{
-                l = mid + 1;
-            }
-        }
-        
-        if(idx == -1) return 0;
-        return vec.size() - idx;
-    }
     int countNegatives(vector<vector<int>>& grid) {
         int count = 0;
-        for(auto x: grid){
-            count += binary(x);
+        int n = grid[0].size();
+        int currRowNegativeIndex = n - 1;
+
+        // Iterate on all rows of the matrix one by one.
+        for (auto& row : grid) {
+            // Decrease 'currRowNegativeIndex' so that it points to current row's last positive element.
+            while (currRowNegativeIndex >= 0 && row[currRowNegativeIndex] < 0) {
+                currRowNegativeIndex--;
+            }
+            // 'currRowNegativeIndex' points to the last positive element,
+            // which means 'n - (currRowNegativeIndex + 1)' is the number of all negative elements.
+            count += (n - (currRowNegativeIndex + 1));
         }
         return count;
     }
