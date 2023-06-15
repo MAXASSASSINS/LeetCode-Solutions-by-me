4c036b286f704d6e29bfaf6f123b9149279e55c2
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2023-06-15 14:30:12 +0530

    Time: 208 ms (51.38%), Space: 107.2 MB (84.30%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cpp
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cpp
@@ -1,49 +1,35 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     int maxLevelSum(TreeNode* root) {
-        queue<pair<TreeNode*,int>> q;
-        unordered_map<int, int> m;
-        int maxi = root->val;
-        int maxIndex = 1;
-        q.push({root, 1});
-        
-        while(!q.empty()){
-            auto node = q.front();
-            m[node.second] += node.first->val;
-            q.pop();
-            
-            if(node.first->left){   
-                q.push({node.first->left, node.second + 1});
+        int maxSum = INT_MIN;
+        int ans = 0, level = 0;
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            level++;
+            int sumAtCurrentLevel = 0;
+            // Iterate over all the nodes in the current level.
+            for (int sz = q.size(); sz > 0; --sz) {
+                TreeNode* node = q.front();
+                q.pop();
+                sumAtCurrentLevel += node->val;
+
+                if (node->left != nullptr) {
+                    q.push(node->left);
+                }
+                if (node->right != nullptr) {
+                    q.push(node->right);
+                }
             }
-            if(node.first->right){
-                q.push({node.first->right, node.second + 1});
+
+            if (maxSum < sumAtCurrentLevel) {
+                maxSum = sumAtCurrentLevel;
+                ans = level;
             }
-                
         }
-        
-        for(auto it: m){
-            if(maxi < it.second){
-                maxi = it.second;
-                maxIndex = it.first;
-            }
-            else if(maxi == it.second && maxIndex > it.first ){
-                maxIndex = it.first;
-            }
-        }
-        
-        return maxIndex;
-        
-        
+
+        return ans;
     }
 };
