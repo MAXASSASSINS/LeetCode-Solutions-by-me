6b16626a6a191dcb8f61f96be0c61a5f4a82f017
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2023-06-20 13:25:37 +0530

    Time: 275 ms (24.25%), Space: 136.6 MB (42.00%) - LeetHub

--- 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.cpp
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.cpp
@@ -1,26 +1,35 @@
 class Solution {
 public:
     vector<int> getAverages(vector<int>& nums, int k) {
-        vector<long long> prefix(nums.size() + 1);
-        
-        for(int i = 0; i < nums.size(); i++){
-            prefix[i + 1] += (long long)prefix[i] + nums[i];
+        // When a single element is considered then its averafge will be the number itself only.
+        if (k == 0) {
+            return nums;
+        }
+
+        int n = nums.size();
+        vector<int> averages(n, -1);
+
+        // Any index will not have 'k' elements in it's left and right.
+        if (2 * k + 1 > n) {
+            return averages;
+        }
+
+        // Generate 'prefix' array for 'nums'.
+        // 'prefix[i + 1]' will be sum of all elements of 'nums' from index '0' to 'i'.
+        vector<long long> prefix(n + 1);
+        for (int i = 0; i < n; ++i) {
+            prefix[i + 1] = prefix[i] + nums[i];
         }
         
-        
-        vector<int> avgs(nums.size());
-        for(int i = 0; i < nums.size(); i++){
-            if(i - k < 0 || (i + k) >= nums.size()){
-                avgs[i] = -1;
-                continue;
-            }
-            long long left = prefix[i - k];
-            long long right = prefix[i + k + 1];
-            // cout<<right<<" ";
-            avgs[i] = (int)((right - left)/(2*k + 1));
-            // cout<<avgs[i]<<"\t";
+        // We iterate only on those indices which have atleast 'k' elements in their left and right.
+        // i.e. indices from 'k' to 'n - k'
+        for (int i = k; i < (n - k); ++i) {
+            int leftBound = i - k, rightBound = i + k;
+            long long subArraySum = prefix[rightBound + 1] - prefix[leftBound];
+            int average = subArraySum / (2 * k + 1);
+            averages[i] = average;
         }
-        
-        return avgs;
+
+        return averages;
     }
 };
