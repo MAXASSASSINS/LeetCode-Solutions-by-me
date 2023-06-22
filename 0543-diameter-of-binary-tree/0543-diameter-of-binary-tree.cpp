86463f5250b9a3a7829669063e3b42d51513136c
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2023-06-22 13:40:19 +0530

    Time: 8 ms (80.12%), Space: 20.2 MB (82.09%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -9,7 +9,7 @@ class Solution {
         int lHeight = height(root->left);
         int rHeight = height(root->right);
 
-        ans= max(ans, 1 + lHeight + rHeight);
+        ans= max(ans, lHeight + rHeight);
         return 1+ max( lHeight , rHeight);
 
     }
@@ -17,6 +17,6 @@ class Solution {
     int diameterOfBinaryTree(TreeNode* root) {
         if(!root) return 0;
         height(root);
-        return ans-1;
+        return ans;
     }
 };
