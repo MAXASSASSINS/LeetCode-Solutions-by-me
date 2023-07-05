48c295179b4b68d9b163c5687d250b54abb9325f
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2023-07-05 14:25:35 +0530

    Time: 27 ms (65.34%), Space: 13.4 MB (72.81%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,20 +1,38 @@
 class Solution {
-    public:
-bool isBipartite(vector<vector<int>>& graph) {
-	vector<int> vis(graph.size());
-	for(int i = 0; i < graph.size(); i++)
-		if(!dfs(graph, vis, i, vis[i]))
-			return false;
-	return true;
-}
+public:
+    bool bfs(vector<vector<int>>& graph, vector<int> &color, int st){
+        queue<int> q;
+        q.push(st);
+        color[st] = 0;
+        
+         while(!q.empty()){
+            int u = q.front();
+            int clr = color[u];
+            q.pop();
+            
+            for(auto v: graph[u]){
+                if(color[v] == -1){
+                    color[v] = !clr;
+                    q.push(v);
+                }
+                else if(color[v] == clr) return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<int> color(n, -1);
+        
+        for(int i = 0; i < n; i++){
+            if(color[i] == -1){
+                if(bfs(graph, color, i) == false) return false;       
+            }
+        }        
 
-bool dfs(vector<vector<int>> &graph, vector<int> &vis, int i, int pos) {
-	if(!vis[i]) {
-		vis[i] = pos == 2 ? 2 : 1;
-		for(int j : graph[i])
-			if(!dfs(graph, vis, j, pos == 2 ? 1 : 2))
-				return false;
-	}
-	return !pos || pos == vis[i];
-}
+        return true;        
+    }
 };
