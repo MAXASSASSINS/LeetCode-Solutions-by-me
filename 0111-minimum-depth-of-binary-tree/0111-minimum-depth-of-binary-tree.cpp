3eb656892aaef5cf2b7800d4cd3b847334c906a3
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2023-07-10 11:56:33 +0530

    Time: 314 ms (16.41%), Space: 144.8 MB (31.37%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
@@ -11,7 +11,10 @@
  */
 class Solution {
 public:
-    int minDepth(TreeNode* root) {
+    
+    int ans = INT_MAX;
+    
+    int bfs(TreeNode *root){
         if(!root) return 0;
         queue<TreeNode *> q;
         q.push(root);
@@ -35,5 +38,27 @@ public:
         }
         
         return level;
+    }
+    
+    int dfs(TreeNode* root) {
+        if (root == NULL) {
+            return 0;
+        }
+        
+        // If only one of child is non-null, then go into that recursion.
+        if (!root->left) {
+            return 1 + dfs(root->right);
+        } else if (!root->right) {
+            return 1 + dfs(root->left);
+        }
+        
+        // Both children are non-null, hence call for both childs.
+        return 1 + min(dfs(root->left), dfs(root->right));
+    }
+    
+    int minDepth(TreeNode* root) {
+        // return bfs(root);
+        return dfs(root);
+        
     }
 };
