98106766078c75351053aea5e41e758b8d53e38f
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2023-07-10 14:08:01 +0530

    Time: 24 ms (74.15%), Space: 13.3 MB (69.36%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,30 +1,42 @@
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<vector<pair<int, int>>> adj(n);
-        for (auto& e : flights) {
-            adj[e[0]].push_back({e[1], e[2]});
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {             
+        vector<pair<int,int>> adj[n];
+        
+        for(int i = 0; i < flights.size(); i++){
+            adj[flights[i][0]].push_back({flights[i][1], flights[i][2]});    
         }
-        vector<int> dist(n, numeric_limits<int>::max());
-        queue<pair<int, int>> q;
-        q.push({src, 0});
-        int stops = 0;
-
-        while (stops <= k && !q.empty()) {
-            int sz = q.size();
-            // Iterate on current level.
-            while (sz--) {
-                auto [node, distance] = q.front();
-                q.pop();
-                // Iterate over neighbors of popped node.
-                for (auto& [neighbour, price] : adj[node]) {
-                    if (price + distance >= dist[neighbour]) continue;
-                    dist[neighbour] = price + distance;
-                    q.push({neighbour, dist[neighbour]});
+        
+        vector<int> dis(n, INT_MAX);
+        dis[src] = 0;
+        
+        // priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+        queue<vector<int>> pq;
+        pq.push({src, 0, 0});
+        
+        while(!pq.empty()){
+            auto it = pq.front();
+            pq.pop();
+            
+            int node = it[0];
+            int distance = it[1];
+            int hop = it[2];
+            
+            for(auto pair: adj[node]){
+                int adjNode = pair.first;
+                int egWt = pair.second;
+                
+                int newDis = distance + egWt;
+                
+                if(dis[adjNode] > newDis && hop <= k){
+                    dis[adjNode] = newDis;
+                    pq.push({adjNode, newDis, hop + 1});
                 }
             }
-            stops++;
         }
-        return dist[dst] == numeric_limits<int>::max() ? -1 : dist[dst];
+        
+        return dis[dst] == INT_MAX ? -1 : dis[dst];
+        
+        
     }
 };
