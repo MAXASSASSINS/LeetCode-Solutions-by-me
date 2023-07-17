080dfa30307efb1bd74d6a3bf0ab02521072edc9
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2023-07-17 14:47:03 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (75.43%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -35,12 +35,36 @@ public:
         
     }
     
+    int spaceOptimisation(int m, int n){
+        vector<int> prev(n);
+        
+        prev[0] = 1;
+        
+        for(int i = 0; i < m; i++){
+            vector<int> curr(n);
+            for(int j = 0; j < n; j++){
+                if(i == 0 && j == 0){
+                    curr[j] = 1;
+                    continue;
+                }
+                int left = j > 0 ? curr[j - 1] : 0;
+                int top = i > 0 ? prev[j] : 0;
+                curr[j] = left + top;
+            }
+            
+            prev = curr;
+        }
+        
+        return prev[n - 1];
+    }
+    
     int uniquePaths(int m, int n) {
         // return solve(m - 1,n - 1);
         
         // vector<vector<int>> dp(m, vector<int>(n, -1));
         // return solveMem(m - 1, n - 1, dp);
         
-        return solveTab(m, n);
+        // return solveTab(m, n);
+        return spaceOptimisation(m, n);
     }
 };
