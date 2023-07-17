3a23d3e523743600ddd9c3a2213befc61c937a20
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2023-07-17 14:39:20 +0530

    Time: 5 ms (22.84%), Space: 6.5 MB (20.40%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -17,10 +17,30 @@ public:
         return dp[m][n] = left + top;
     }
     
+    int solveTab(int m, int n){
+        vector<vector<int>> dp(m, vector<int>(n));
+        
+        dp[0][0] = 1;
+        
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(i == 0 && j == 0) continue;
+                int left = j > 0 ? dp[i][j - 1] : 0;
+                int top = i > 0 ? dp[i - 1][j] : 0;
+                dp[i][j] = left + top;
+            }
+        }
+        
+        return dp[m - 1][n - 1];
+        
+    }
+    
     int uniquePaths(int m, int n) {
         // return solve(m - 1,n - 1);
         
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        return solveMem(m - 1, n - 1, dp);
+        // vector<vector<int>> dp(m, vector<int>(n, -1));
+        // return solveMem(m - 1, n - 1, dp);
+        
+        return solveTab(m, n);
     }
 };
