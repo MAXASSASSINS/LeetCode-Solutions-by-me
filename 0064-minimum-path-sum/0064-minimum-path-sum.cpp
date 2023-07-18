e47b3297d8aad80c84c1317f3c743d36d3128d58
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2023-07-18 17:56:57 +0530

    Time: 12 ms (55.87%), Space: 10.1 MB (43.83%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -20,12 +20,41 @@ public:
         return dp[m][n] = min(top, left);
     }
     
+    int solveTab(vector<vector<int>> &grid){
+        int m = grid.size();
+        int n = grid[0].size();
+        
+        vector<vector<int>> dp(m, vector<int>(n));
+        
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(i == 0 && j == 0){
+                    dp[i][j] = grid[0][0];
+                }
+                else{
+                    int left = INT_MAX;
+                    int top = INT_MAX;
+                    if(j > 0)
+                        left = grid[i][j] +  dp[i][j - 1];
+                    if(i > 0)
+                        top = grid[i][j] + dp[i - 1][j];
+                    dp[i][j] = min(top, left);
+                }
+            }
+        }
+        
+        return dp[m - 1][n - 1];
+        
+    }
+    
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
         // return solve(grid, m - 1, n - 1);   
         
-        vector<vector<int>> dp(m, vector<int>(n, - 1));
-        return solveMem(grid, m - 1, n - 1, dp);
+        // vector<vector<int>> dp(m, vector<int>(n, - 1));
+        // return solveMem(grid, m - 1, n - 1, dp);
+        
+        return solveTab(grid);
     }
 };
