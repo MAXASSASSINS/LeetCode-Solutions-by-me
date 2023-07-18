0c24702e5c44d5bd8ac16f9590955d3fef216866
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2023-07-18 17:41:21 +0530

    Time: 11 ms (64.68%), Space: 10.1 MB (14.28%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,19 +1,31 @@
 class Solution {
 public:
+    int solve(vector<vector<int>> &grid, int m, int n){
+        if(m == 0 && n == 0) return grid[0][0];
+        if(m < 0 || n < 0) return 1e9;
+        
+        int left = grid[m][n] + solve(grid, m, n - 1);
+        int top = grid[m][n] + solve(grid, m - 1, n);
+        return min(top, left);
+    }
+    
+    int solveMem(vector<vector<int>> &grid, int m, int n, vector<vector<int>> &dp){
+        if(m == 0 && n == 0) return grid[0][0];
+        if(m < 0 || n < 0) return 1e9;
+        
+        if(dp[m][n] != -1) return dp[m][n];
+        
+        int left = grid[m][n] + solveMem(grid, m, n - 1, dp);
+        int top = grid[m][n] + solveMem(grid, m - 1, n, dp);
+        return dp[m][n] = min(top, left);
+    }
+    
     int minPathSum(vector<vector<int>>& grid) {
-        int rows = grid.size();
-        int cols = grid[0].size();
-        for (int i = 1; i < cols; ++i) {
-            grid[0][i] = grid[0][i - 1] + grid[0][i];
-        }
-        for (int i = 1; i < rows; ++i) {
-            grid[i][0] = grid[i - 1][0] + grid[i][0];
-        }
-        for (int i = 1; i < rows; ++i) {
-            for (int j = 1; j < cols; ++j) {
-                grid[i][j] = grid[i][j] + min(grid[i - 1][j], grid[i][j - 1]);
-            }
-        }
-        return grid[rows - 1][cols - 1];
+        int m = grid.size();
+        int n = grid[0].size();
+        // return solve(grid, m - 1, n - 1);   
+        
+        vector<vector<int>> dp(m, vector<int>(n, - 1));
+        return solveMem(grid, m - 1, n - 1, dp);
     }
 };
