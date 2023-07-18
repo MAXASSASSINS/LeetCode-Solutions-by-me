d22d86381b9b7eee71d0ce49fd6190610886f6d7
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2023-07-18 18:05:57 +0530

    Time: 16 ms (17.79%), Space: 10 MB (70.58%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -47,6 +47,35 @@ public:
         
     }
     
+    int spaceOptimisation(vector<vector<int>> &grid){
+        int m = grid.size();
+        int n = grid[0].size();
+        
+        vector<int> prev(n);
+        
+        for(int i = 0; i < m; i++){
+            vector<int> curr(n);
+            for(int j = 0; j < n; j++){
+                if(i == 0 && j == 0){
+                    curr[j] = grid[0][0];
+                }
+                else{
+                    int left = INT_MAX;
+                    int top = INT_MAX;
+                    if(j > 0)
+                        left = grid[i][j] +  curr[j - 1];
+                    if(i > 0)
+                        top = grid[i][j] + prev[j];
+                    curr[j] = min(top, left);
+                }
+            }
+             prev = curr;
+        }
+        
+        return prev[n - 1];
+        
+    }
+    
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
@@ -55,6 +84,7 @@ public:
         // vector<vector<int>> dp(m, vector<int>(n, - 1));
         // return solveMem(grid, m - 1, n - 1, dp);
         
-        return solveTab(grid);
+        // return solveTab(grid);
+        return spaceOptimisation(grid);
     }
 };
