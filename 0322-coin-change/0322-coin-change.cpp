c9e13c729a4db44c3b903b98ebc76b200049b295
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2023-07-22 13:25:27 +0530

    Time: 166 ms (24.78%), Space: 45 MB (37.63%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -34,15 +34,48 @@ public:
     }
     
     
+    int solveTab(vector<int> &coins, int amount){
+        int n = coins.size();
+        
+        vector<vector<int>> dp(n, vector<int> (amount + 1));
+        
+        for(int amt = 0; amt <= amount; amt++){
+            if(amt % coins[0] == 0){
+                dp[0][amt] = amt / coins[0];
+            }
+            else{
+                dp[0][amt] = 1e8;
+            }
+        }
+        
+        for(int index = 1; index < n; index++){
+            for(int amt = 0; amt <= amount; amt++){
+                int excl = dp[index - 1][amt];
+                int incl = INT_MAX;
+                if(amt >= coins[index]){
+                    incl = 1 + dp[index][amt - coins[index]];
+                }   
+                dp[index][amt] = min(excl, incl);
+            }
+        }
+        
+        return dp[n - 1][amount];
+    }
+    
     
     
     int coinChange(vector<int>& coins, int amount) {
-        // sort(coins.begin(), coins.end());
         int n = coins.size();
         if(amount == 0) return 0;
+        
         // int ans = solve(coins, amount, n - 1);
-        vector<vector<int>> dp(n + 1, vector<int> (amount + 1, -1));
-        int ans = solveMem(coins, amount, n - 1, dp);
+        
+        // vector<vector<int>> dp(n, vector<int> (amount + 1, -1));
+        // int ans = solveMem(coins, amount, n - 1, dp);
+        
+        
+        int ans = solveTab(coins, amount);
+        
         if(ans > 1e4 + 1) return -1;
         return ans;
     }
