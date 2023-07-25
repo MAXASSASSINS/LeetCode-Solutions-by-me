3a6a4774f748233461630d9520d72f3e802e81e7
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2023-07-25 13:06:49 +0530

    Time: 2680 ms (5.92%), Space: 422.6 MB (9.77%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -13,12 +13,12 @@ public:
     }
     
     int solveMem(string s, string t, int i, int j, vector<vector<int>> &dp){
-        if(j < 0) return 1;
-        if(i < 0) return 0;
+        if(j == 0) return 1;
+        if(i == 0) return 0;
         
         if(dp[i][j] != -1) return dp[i][j];
         
-        if(s[i] == t[j]){
+        if(s[i - 1] == t[j - 1]){
             return dp[i][j] = solveMem(s, t, i - 1, j - 1, dp) + solveMem(s, t, i - 1, j, dp);
         }
         else{
@@ -26,14 +26,23 @@ public:
         }
     }
     
+    int solveTab(string s, string t, int n, int m){
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        return 0;
+        
+    }
+    
+    
     int numDistinct(string s, string t) {
         int n = s.length();
         int m = t.length();
     
         // return solve(s, t, n - 1, m - 1);
         
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return solveMem(s, t, n - 1, m - 1, dp);
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
+        return solveMem(s, t, n, m, dp);
+        
+        // return solveTab(s, t, n, m);
         
     }
 };
