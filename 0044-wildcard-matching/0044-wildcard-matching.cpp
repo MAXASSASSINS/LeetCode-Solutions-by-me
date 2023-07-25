a6dc1692c16166f620df8753a1acf52da9a08aae
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2023-07-25 17:23:27 +0530

    Time: 127 ms (11.99%), Space: 11.3 MB (69.71%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -30,9 +30,7 @@ public:
             }
             return true;
         }
-        
         if(dp[i][j] != -1) return dp[i][j];
-        
         if(s[i - 1] == t[j - 1] || t[j - 1] == '?'){
             return dp[i][j] = solveMem(s, t, i - 1, j - 1, dp);
         }
@@ -44,32 +42,45 @@ public:
         }
     }
     
-    int solveTab(string &s, string &t, int n, int m){
-        vector<vector<double>> dp(n + 1, vector<double>(m + 1, 0));
+    bool solveTab(string &s, string &t, int n, int m){
+        vector<vector<bool>> dp(n + 1, vector<bool>(m + 1));
         
-        for(int i = 0; i <= n; i++){
-            dp[i][0] = 1;
+        dp[0][0] = true;
+        for(int j = 1; j <= m; j++){
+            bool flag = true;
+            for(int k = 1; k <= j; k++){
+                if(t[k - 1] != '*'){
+                    flag = false;
+                    break;
+                }
+            }
+            dp[0][j] = flag;
         }
         
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= m; j++){
-                if(s[i - 1] == t[j - 1]){
-                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                if(s[i - 1] == t[j - 1] || t[j - 1] == '?'){
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+                else if(t[j - 1] == '*'){
+                    dp[i][j] = dp[i - 1][j] | dp[i][j - 1];
                 }
                 else{
-                    dp[i][j] = dp[i - 1][j];
+                    dp[i][j] = false;
                 }
             }
         }
         
-        return (int)dp[n][m];       
+        return dp[n][m];       
     }
     bool isMatch(string s, string p) {
         int m = s.length();
         int n = p.length();
         
         // return solve(s, p, m, n);
-        vector<vector<int>> dp(m + 1, vector<int> (n + 1, -1));
-        return solveMem(s, p, m, n, dp);
+        // vector<vector<int>> dp(m + 1, vector<int> (n + 1, -1));
+        // return solveMem(s, p, m, n, dp);
+        
+        return solveTab(s, p, m, n);
     }
 };
