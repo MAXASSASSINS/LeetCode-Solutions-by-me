ef1f86ac140deab0ae3348b17e9cb4066a42d240
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2023-07-27 20:57:36 +0530

    Time: 844 ms (30.37%), Space: 292.2 MB (16.00%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,106 +1,39 @@
 class Solution {
 public:
-    int solve(vector<int> &nums, int curr, int prev) {
-        if(curr >= nums.size()){
-            return 0;
+    int solve(vector<int> &nums, int index, int prev){
+        if(index == nums.size()) return 0;
+        
+        
+        int excl = solve(nums, index + 1, prev);
+        int incl = INT_MIN;
+        
+        if(prev == -1 || nums[prev] < nums[index]){
+            incl = 1 + solve(nums, index + 1, index);
         }
-
-        int include = 0;
-        if(prev == -1 || nums[curr] > nums[prev]){
-            include = 1 + solve(nums, curr + 1, curr);
-        }
-
-        int exclude = 0 + solve(nums, curr + 1, prev);
-        return max(include, exclude);
+        
+        return max(excl, incl);
     }
-
-    int solveMem(vector<int> &nums,vector<vector<int>> &dp, int curr, int prev){
-        if(curr >= nums.size()){
-            return 0;
+    
+    int solveMem(vector<int> &nums, int index, int prev, vector<vector<int>> &dp){
+        if(index == nums.size()) return 0;
+        
+        if(dp[index][prev + 1] != -1) return dp[index][prev + 1];
+        
+        int excl = solveMem(nums, index + 1, prev, dp);
+        int incl = INT_MIN;
+        
+        if(prev == -1 || nums[prev] < nums[index]){
+            incl = 1 + solveMem(nums, index + 1, index, dp);
         }
-
-        if(dp[curr][prev + 1] != -1){
-            return dp[curr][prev + 1];
-        }
-
-        int include = 0;
-        if(prev == -1 || nums[curr] > nums[prev]){
-            include = 1 + solveMem(nums,dp, curr + 1, curr);
-        }
-
-        int exclude = 0 + solveMem(nums,dp, curr + 1, prev);
-        dp[curr][prev + 1] = max(include, exclude);
-        return dp[curr][prev + 1];
+        
+        return dp[index][prev + 1] = max(excl, incl);
     }
-
-    int solveTab(vector<int> &nums) {
-        int n = nums.size();
-        vector<vector<int>> dp(n + 1, vector<int> (n + 1, 0));
-        for (int curr = n - 1; curr >= 0; --curr) {
-            for (int prev = curr - 1; prev >= -1 ; --prev) {
-                int include = 0;
-                if(prev == -1 || nums[curr] > nums[prev]){
-                    include = 1 + dp[curr + 1][curr + 1];
-                }
-
-                int exclude = 0 + dp[curr + 1][prev + 1];
-                dp[curr][prev + 1] = max(include, exclude);
-            }
-        }
-
-        return dp[0][0];
-
-    }
-
-    int solveSO(vector<int> &nums){
-        int n = nums.size();
-        vector<vector<int>> dp(n + 1, vector<int> (n + 1, 0));
-
-        vector<int> currentRow(n + 1, 0);
-        vector<int> nextRow(n + 1, 0);
-
-
-        for (int curr = n - 1; curr >= 0; --curr) {
-            for (int prev = curr - 1; prev >= -1 ; --prev) {
-                int include = 0;
-                if(prev == -1 || nums[curr] > nums[prev]){
-                    include = 1 + nextRow[curr + 1];
-                }
-
-                int exclude = 0 + nextRow[prev + 1];
-                currentRow[prev + 1] = max(include, exclude);
-            }
-            nextRow = currentRow;
-        }
-
-        return currentRow[0];
-    }
-
-    int solveOptimal(vector<int> &nums) {
-        int n = nums.size();
-        if (nums.size() == 0)
-            return 0;
-        vector<int> ans;
-        ans.push_back(nums[0]);
-        for (int i = 1; i < n; ++i) {
-            if (nums[i] > ans.back()){
-                ans.push_back(nums[i]);
-            }
-            else{
-                int index = lower_bound(ans.begin(), ans.end(), nums[i]) - ans.begin();
-                ans[index] = nums[i];
-            }
-        }
-        return ans.size();
-    }
-
     int lengthOfLIS(vector<int>& nums) {
-//        return solve(nums, 0, -1);
+        // return solve(nums, 0, -1);
+        
         int n = nums.size();
-        vector<vector<int>>dp (n , vector<int> (n + 1, -1));
-//        return solveMem(nums, dp, 0, -1);
-//        return solveTab(nums);
-//        return solveSO(nums);
-        return solveOptimal(nums);
+        vector<vector<int>> dp(n, vector<int> (n + 1, -1));
+        return solveMem(nums, 0, -1, dp);
+        
     }
 };
