4f5a1fc96868188d48c5c3981f389bdac5e9986c
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2023-07-28 16:14:01 +0530

    Time: 135 ms (31.34%), Space: 7.6 MB (29.30%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -9,9 +9,25 @@ public:
         return max(left, right);
     }
     
+    int solveMem(vector<int> &nums, int i, int j, vector<vector<int>> &dp){
+        if(i > j) return 0;
+        
+        if(dp[i][j] != -1) return dp[i][j];
+        
+        int left = nums[i] + min(solve(nums, i + 2, j), solve(nums, i + 1, j - 1));
+        int right = nums[j] + min(solve(nums, i, j - 2), solve(nums, i + 1, j - 1));
+        
+        return dp[i][j] = max(left, right);
+    }
+    
+
+    
     bool PredictTheWinner(vector<int>& nums) {
         int n = nums.size();
-        int p1 = solve(nums, 0, n - 1);
+        // int p1 = solve(nums, 0, n - 1);
+        
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+        int p1 = solveMem(nums, 0, n - 1, dp);
         
         int total = 0;
         for(auto x: nums) total += x;
