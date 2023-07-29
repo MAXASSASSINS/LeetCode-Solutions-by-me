78f99038f592cf2bbe58b6920f031605fb7f5afa
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2023-07-29 09:32:53 +0530

    Time: 503 ms (43.20%), Space: 572 MB (8.08%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -28,12 +28,36 @@ public:
         
         return dp[index][prev + 1] = max(excl, incl);
     }
+    
+    int solveTab(vector<int> &nums){
+        int n = nums.size();
+        vector<vector<int>> dp(n + 1, vector<int> (n + 1, 0));
+        
+        for(int index = n - 1; index >= 0; index--){
+            for(int prev = index - 1; prev >= -1; prev--){
+                int excl = dp[index + 1][prev + 1];
+                int incl = INT_MIN;
+
+                if(prev == -1 || nums[prev] < nums[index]){
+                    incl = 1 + dp[index + 1][index + 1];
+                }
+
+                dp[index][prev + 1] = max(excl, incl);
+            }
+        }
+        
+        
+        return dp[0][0];
+    }    
+    
     int lengthOfLIS(vector<int>& nums) {
         // return solve(nums, 0, -1);
         
         int n = nums.size();
         vector<vector<int>> dp(n, vector<int> (n + 1, -1));
-        return solveMem(nums, 0, -1, dp);
+        // return solveMem(nums, 0, -1, dp);
+        
+        return solveTab(nums);
         
     }
 };
