1391cc9d80d53e45ed497b5f996dddff767e9c92
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2023-08-05 21:53:59 +0530

    Time: 15 ms (96.35%), Space: 19.1 MB (69.04%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,10 +1,23 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-    for (int lastNonZeroFoundAt = 0, cur = 0; cur < nums.size(); cur++) {
-        if (nums[cur] != 0) {
-            swap(nums[lastNonZeroFoundAt++], nums[cur]);
+        int j = -1;
+        int n = nums.size();
+        for(int i = 0; i < n; i++){
+            if(nums[i] == 0){
+                j = i;
+                break;
+            }
         }
+        
+        if(j == -1) return;
+        for(int i = j + 1; i < n; i++){
+            if(nums[i] != 0){
+                swap(nums[i], nums[j]);
+                j++;
+            }
+        }
+        
+        
     }
-}
 };
