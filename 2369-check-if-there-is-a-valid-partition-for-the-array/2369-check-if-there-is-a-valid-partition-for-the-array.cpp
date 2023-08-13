31944df9c8fd52233b1ea3a0b62a5502f9af96f7
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2023-08-13 14:00:49 +0530

    Time: 113 ms (98.46%), Space: 92.2 MB (37.85%) - LeetHub

---
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    bool solve(vector<int> &nums, int index){
+        if(index < 0){
+            return true;
+        }
+        
+        bool twoEqual = false;
+        bool threeEqual = false;
+        bool threeIncreasing = false;
+        
+        if(index > 0 && nums[index] == nums[index - 1]){
+            twoEqual = solve(nums, index - 2);
+        }
+        
+        if(index > 1 && nums[index] == nums[index - 1] && nums[index] == nums[index - 2]){
+            threeEqual = solve(nums, index - 3);
+        }
+        
+        if(index > 1 && nums[index] == nums[index - 1] + 1 && nums[index - 1] == nums[index - 2] + 1){
+            threeIncreasing = solve(nums, index - 3);
+        }
+        
+        return twoEqual || threeEqual || threeIncreasing;
+        
+    }
+    
+    bool solveMem(vector<int> &nums, int index, vector<int> &dp){
+        if(index < 0){
+            return true;
+        }
+        
+        if(dp[index] != -1) return dp[index];
+        
+        bool twoEqual = false;
+        bool threeEqual = false;
+        bool threeIncreasing = false;
+        
+        if(index > 0 && nums[index] == nums[index - 1]){
+            twoEqual = solveMem(nums, index - 2, dp);
+        }
+        
+        if(index > 1 && nums[index] == nums[index - 1] && nums[index] == nums[index - 2]){
+            threeEqual = solveMem(nums, index - 3, dp);
+        }
+        
+        if(index > 1 && nums[index] == nums[index - 1] + 1 && nums[index - 1] == nums[index - 2] + 1){
+            threeIncreasing = solveMem(nums, index - 3, dp);
+        }
+        
+        return dp[index] = twoEqual || threeEqual || threeIncreasing;
+        
+    }
+    
+    bool validPartition(vector<int>& nums) {
+        int n = nums.size();
+        // return solve(nums, n - 1);
+        
+        vector<int> dp(n, -1);
+        return solveMem(nums, n - 1, dp);
+    }
+};
