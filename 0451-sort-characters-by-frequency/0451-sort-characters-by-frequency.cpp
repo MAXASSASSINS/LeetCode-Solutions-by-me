b05a9e0205d9d98686beea233887fbfaaf311bd7
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2023-08-17 19:07:31 +0530

    Time: 12 ms (61.86%), Space: 9.4 MB (8.97%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,38 +1,19 @@
 class Solution {
 public:
     string frequencySort(string s) {
-        unordered_map<char , int> lower;
-        unordered_map<char , int> upper;
-        for(int i = 0; i < s.length(); i++){
-            char c = s.at(i);
-            if(islower(c)){
-                lower[c]++;
-            }
-            else{
-                upper[c]++;
-            }
+        vector<pair<int, char>> hash(128, {0,0});
+        
+        for(char c: s){
+            hash[c] = {hash[c].first + 1, c};
         }
-        multimap<int , char, greater<int>> m1;
-        auto it = lower.begin();
-
-        while (it != lower.end()){
-            m1.insert(make_pair(it->second, it->first));
-            it++;
+        
+        sort(hash.begin(), hash.end());
+        string res;
+        for(int i = 0; i < 128; i++){
+            res = string(hash[i].first, hash[i].second) + res;
         }
-        it = upper.begin();
-        while (it != upper.end()){
-            m1.insert(make_pair(it->second, it->first));
-            it++;
-        }
-        string t;
-        auto it2 = m1.begin();
-        while (it2 != m1.end()){
-            for(int i = 0; i < it2->first; i++){
-                t += it2->second;
-            }
-            it2++;
-        }
-        return t;
-
+        
+        return res;
+        
     }
 };
