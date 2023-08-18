2aa5298783063950589a38ea315300c78bcbdd6f
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2023-08-18 08:50:01 +0530

    Time: 112 ms (38.28%), Space: 39.5 MB (24.34%) - LeetHub

---
+++ 1615-maximal-network-rank/1615-maximal-network-rank.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maximalNetworkRank(int n, vector<vector<int>>& roads) {
+        unordered_map<int, set<int>> adj;
+        
+        int maxi = 0;
+        
+        for(int i = 0; i < roads.size(); i++){
+            adj[roads[i][0]].insert(roads[i][1]);
+            adj[roads[i][1]].insert(roads[i][0]);
+        }
+        
+        for(int i = 0; i < n - 1; i++){
+            for(int j = i + 1; j < n; j++){
+                int count = 0;
+                auto it = adj[i].find(j);
+                if(it != adj[i].end()){
+                    count -= 1;
+                }
+                count += adj[i].size();
+                count += adj[j].size();
+                maxi = max(maxi, count);
+            }
+        }
+        
+        return maxi;
+    }
+};
