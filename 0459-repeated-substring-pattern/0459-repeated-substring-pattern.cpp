bde148f0f1014dbfb5d0fcf643e2ef9c4ad0e28c
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2023-08-21 08:43:06 +0530

    Time: 16 ms (79.18%), Space: 13.5 MB (46.16%) - LeetHub

---
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        string S = s + s;
+        S = S.substr(1, S.size() - 2);
+        cout<<S;
+        return S.find(s) == string::npos ? false : true;
+    }
+};
