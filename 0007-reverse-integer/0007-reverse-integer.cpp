6da023749d537a3e0a7aef2ed1d5deffca88f9b2
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2023-08-23 13:08:42 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (46.49%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,30 +1,12 @@
-class Solution {
+class Solution {                      
 public:
     int reverse(int x) {
-        int ans = 0;
-        bool xIsNegative = false;
-        if (x < 0){
-            xIsNegative = true;
-            if (x == INT_MIN)
-                return 0;
-            x *= -1;
-        }
-        while (x != 0){
-            if(ans <= INT_MAX/10)
-                ans *= 10;
-            else
-                return 0;
-            if(ans <= INT_MAX - x % 10)
-                ans += x % 10;
-            else
-                return 0;
-
-            x /= 10;
-        }
-
-        if (xIsNegative)
-            ans *= -1;
-
-        return  ans;
+        int r=0;      // decleare r 
+        while(x){
+         if (r>INT_MAX/10 || r<INT_MIN/10) return 0; // check 32 bit range if r is outside the range then return 0 
+         r=r*10+x%10; // find remainder and add its to r
+         x=x/10;     // Update the value of x
+        } 
+        return r;  // if r in the 32 bit range then return r
     }
-};
+}; 
