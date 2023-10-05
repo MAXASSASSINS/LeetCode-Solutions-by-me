8a90eb712d20b45f82712069f14e97f329cf9bb8
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2023-10-05 10:03:05 +0530

    Time: 7 ms (87.90%), Space: 16.3 MB (9.38%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,49 +1,50 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int n1 = -1, n2 = -1;   
+        int el1 = INT_MAX;
+        int el2 = INT_MAX;
         int c1 = 0, c2 = 0;
-        int n = nums.size();
-        for(auto num: nums){
-            if(num == n1){
+        
+        for(int i = 0; i < nums.size(); i++){
+            if(c1 == 0 && nums[i] != el2){
+                c1 = 1;
+                el1 = nums[i];
+            }
+            else if(c2 == 0 && nums[i] != el1){
+                c2 = 1;
+                el2 = nums[i];
+            }
+            else if(nums[i] == el1){
                 c1++;
             }
-            else if(num == n2){
+            else if(nums[i] == el2){
                 c2++;
             }
-            else if(c1 == 0){
-                n1 = num;
-                c1 = 1;
-            }
-            else if(c2 == 0){
-                n2 = num;
-                c2 = 1;
-            }
-            else{ 
+            else{
                 c1--;
                 c2--;
             }
         }
-
-        c1 = 0;
-        c2 = 0;
-        for(int i = 0; i < n; i++){
-            if(nums[i] == n1){
+        
+        c1 = 0, c2 = 0;
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] == el1){
                 c1++;
             }
-            if(nums[i] == n2){
+            else if(nums[i] == el2){
                 c2++;
             }
         }
+        
         vector<int> ans;
-        
-        if(c1 > n /3){
-            ans.push_back(n1);
+        if(c1 > nums.size() / 3){
+            ans.push_back(el1);
+        }
+        if(c2 > nums.size() / 3){
+            ans.push_back(el2);
         }
         
-        if(n1 != n2 && c2 > n/ 3){
-            ans.push_back(n2);
-        }
         return ans;
     }
+    
 };
