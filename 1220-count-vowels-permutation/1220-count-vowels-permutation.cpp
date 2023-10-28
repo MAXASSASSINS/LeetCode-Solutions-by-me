f84ae22d6da2358ec4a96bde62c669c77354abbc
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2023-10-28 11:37:21 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (80.00%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -1,48 +1,20 @@
 class Solution {
 public:
     int countVowelPermutation(int n) {
-        if(n == 1) return 5;
+        const int MOD = 1e9 + 7;
         
-        int mod = 1e9 + 7;
+        long long a = 1, e = 1, i = 1, o = 1, u = 1;
         
-        unordered_map<char, unordered_set<char>> rules;
-        
-        rules['a'] = {'e'};
-        rules['e'] = {'a', 'i'};
-        rules['i'] = {'a', 'e', 'o', 'u'};
-        rules['o'] = {'i', 'u'};
-        rules['u'] = {'a'};
-        
-        
-        unordered_map<char, int> curr, next;
-        curr['a'] = 1;
-        curr['e'] = 1;
-        curr['i'] = 1;
-        curr['o'] = 1;
-        curr['u'] = 1;
-        
-        vector<char> vowels = {'a', 'e', 'i', 'o', 'u'};
-        
-        int total = 0;        
-        for(int i = 2; i <= n; i++){
-            total = 0;
-            for(int j = 0; j < 5; j++){
-                char c = vowels[j];
-                int cnt = curr[c];
-                auto rule = rules[c];
-                total += (cnt * rule.size()) % mod;
-                total %= mod;
-                for(auto it = rule.begin(); it != rule.end(); it++){
-                    char x = *it;
-                    next[x] = (next[x] % mod + cnt % mod) % mod;
-                }
-            }
-            curr = next;
-            next.clear();
+        for (int j = 1; j < n; j++) {
+            long long a_next = e;
+            long long e_next = (a + i) % MOD;
+            long long i_next = (a + e + o + u) % MOD;
+            long long o_next = (i + u) % MOD;
+            long long u_next = a;
             
+            a = a_next, e = e_next, i = i_next, o = o_next, u = u_next;
         }
         
-        
-        return total % mod;
+        return (a + e + i + o + u) % MOD;
     }
 };
