076ac4e39bf1d574c1fdb795d3961f8734daec13
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2024-04-04 20:27:57 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (14.40%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -2,18 +2,16 @@ class Solution {
 public:
     int maxDepth(string s) {
         int ans = 0;
+
         stack<char> st;
-        for(auto c: s){
-            if(c == '('){
-                st.push('(');
-                ans = max(ans, (int)st.size());
-            }
-            else if(c == ')'){
+        for (char c : s) {
+            if (c == '(') {
+                st.push(c);
+            } else if (c == ')') {
                 st.pop();
             }
-            else{
-                continue;
-            }
+            
+            ans = max(ans, (int)st.size());
         }
         
         return ans;
