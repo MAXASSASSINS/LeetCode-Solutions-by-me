1ba83dd4b1420ee74a7f701b963b1f3c2a0525c9
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2024-04-14 13:49:29 +0530

    Time: 3 ms (56.94%), Space: 14.8 MB (48.75%) - LeetHub

---
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void helper(TreeNode *root, int &sum){
+        if(root == nullptr)
+            return;
+        if(root->left != nullptr && root->left->left == nullptr && root->left->right == nullptr){
+            sum += root->left->val;
+        }
+        helper(root->left, sum);
+        helper(root->right, sum);
+    }
+    int sumOfLeftLeaves(TreeNode* root) {
+        int sum = 0;
+        helper(root, sum);
+        return sum;
+    }
+};
