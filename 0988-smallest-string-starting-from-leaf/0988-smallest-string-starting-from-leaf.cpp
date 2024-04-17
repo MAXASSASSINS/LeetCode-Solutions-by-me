b6ea2e1ad34bf28fb82306ad4bd64a996a1feeb3
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2024-04-18 00:24:17 +0530

    Time: 4 ms (91.27%), Space: 20.4 MB (51.36%) - LeetHub

--- 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.cpp
+++ 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.cpp
@@ -12,39 +12,24 @@
 class Solution {
 public:
     
-    void getAllStrings(TreeNode *root, vector<vector<int>> &ans, vector<int> s){
+    void getAllStrings(TreeNode *root, string &ans, string s){
         if(!root->left && !root->right){
-            s.push_back(root->val);
-            reverse(s.begin(), s.end());
-            ans.push_back(s);
-            return;
+            s += 'a' + root->val;
+            string t;
+            for(int i = s.length() - 1; i >= 0; i--){
+                t += s[i];
+            }
+            if(ans == "" || t < ans) ans = t;
         } 
-        s.push_back(root->val);
+        s += 'a' + root->val;
         if(root->left) getAllStrings(root->left, ans, s);
         if(root->right) getAllStrings(root->right, ans, s);
 
     }
     
     string smallestFromLeaf(TreeNode* root) {
-        vector<vector<int>> res;
-        vector<int> temp;
-        getAllStrings(root, res,temp);
-        
-        for(auto vec: res){
-            string s;
-            for(auto x: vec){
-                s += 'a' + x;
-            }
-            cout<<s<<"\t";
-        }
-        
-        sort(res.begin(), res.end());
-        
         string ans;
-        for(auto x: res[0]){
-                ans += 'a' + x;
-        }
-        
+        getAllStrings(root, ans, "");
         return ans;
     }
 };
