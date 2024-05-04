b2a784efc37b4d4c9bbc8ebdac6243f7df6773dc
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2024-05-04 13:43:37 +0530

    Time: 53 ms (79.66%), Space: 45.4 MB (85.44%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.cpp
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -2,21 +2,23 @@ class Solution {
 public:
     int numRescueBoats(vector<int>& people, int limit) {
         sort(people.begin(), people.end());
-        int ans = 0;
-        int l = 0;
-        int h = people.size() -1;
-        while(l < h){
-            int sum = people[l] + people[h];
-            if(sum > limit){
-                h--;
+        int i = 0, j = people.size() - 1;
+        
+        int boats = 0;
+        int ptsf = 0; // peopleTravelledSoFar --> ptsf
+        while(ptsf < people.size()){
+            if(people[i] + people[j] <= limit){
+                i++;
+                j--;
+                ptsf+=2;
             }
             else{
-                l++;
-                h--;
+                j--;
+                ptsf++;
             }
-            ans++;
+            boats++;
         }
-        if(l == h) ans++;
-        return ans;
+        
+        return boats;
     }
 };
