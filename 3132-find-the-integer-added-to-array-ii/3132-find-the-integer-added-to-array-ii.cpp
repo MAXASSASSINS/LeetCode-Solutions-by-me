6959031d303b2374ab873f286e30aefb9558330a
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2024-05-05 12:55:39 +0530

    Time: 63 ms (50.79%), Space: 32.4 MB (61.69%) - LeetHub

--- 3132-find-the-integer-added-to-array-ii/3132-find-the-integer-added-to-array-ii.cpp
+++ 3132-find-the-integer-added-to-array-ii/3132-find-the-integer-added-to-array-ii.cpp
@@ -15,18 +15,20 @@ public:
                 nums1[i] = nums1[j] = INT_MAX;
                 
                 unordered_set<int> s;
+                int x = INT_MIN;
                 int l = 0;
                 for(int i = 0; i < nums1.size(); i++){
                     if(nums1[i] == INT_MAX) continue;
-                    s.insert(nums2[l] - nums1[i]);
+                    int temp = nums2[l] - nums1[i];
+                    if(x == INT_MIN) x = temp;
+                    else if(x != temp){
+                        x = INT_MIN;
+                        break;
+                    }
                     l++;
                 }
-                cout<<s.size()<<"\t";
-                if(s.size() == 1){
-                    auto it = s.begin();
-                    if(ans > *it){
-                        ans = *it;
-                    }
+                if(x != INT_MIN && ans > x){
+                    ans = x;
                 }
                 nums1[i] = temp1;
                 nums1[j] = temp2;
