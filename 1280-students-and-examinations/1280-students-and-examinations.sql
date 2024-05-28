db6fa821768373841f976af49919b07492656493
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2024-05-28 17:52:17 +0530

    Time: 2774 ms (7.99%), Space: 0B (100.00%) - LeetHub

--- 1280-students-and-examinations/1280-students-and-examinations.sql
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -1,40 +1,6 @@
-# Write your MySQL query statement below
-
-# select 
-#     e.student_id, s.student_name, e.subject_name
-# from 
-#     Examinations as e
-# left join
-#     Students as s
-# on
-#     s.student_id = e.student_id
-# order by 
-#     e.student_id, e.subject_name;
-
-
-
-
-
-select t1.student_id, t1.student_name, t1.subject_name, COALESCE(t2.attended_exams, 0) AS attended_exams
-from (
-    select 
-        s.student_id, s.student_name, su.subject_name
-    from 
-        students as s
-    cross join
-        subjects as su
-    order by s.student_id, su.subject_name
-) as t1
-left join (
-    select 
-        student_id, subject_name, count(subject_name) as attended_exams
-    from
-        examinations
-    group by
-        student_id, subject_name
-) as t2
-on
-    t1.student_id = t2.student_id
-    and t1.subject_name = t2.subject_name
-order by
-    t1.student_id, t1.subject_name;
+SELECT s.student_id, s.student_name, sub.subject_name, COUNT(e.student_id) AS attended_exams
+FROM Students s
+CROSS JOIN Subjects sub
+LEFT JOIN Examinations e ON s.student_id = e.student_id AND sub.subject_name = e.subject_name
+GROUP BY s.student_id, s.student_name, sub.subject_name
+ORDER BY s.student_id, sub.subject_name;
