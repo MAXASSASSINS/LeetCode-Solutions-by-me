ac81fb2ab65bf90d916b094b9d62a2f6ee37e9e6
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2024-05-28 18:56:17 +0530

    Time: 1349 ms (47.52%), Space: 0B (100.00%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,44 +1,9 @@
-
-# select 
-#     s.user_id
-# from
-#     signups as s
-# left join
-# ( 
-# )
-
-
 select 
-    s.user_id, round(COALESCE(t3.confirmedCnt/t3.total, 0),2) as confirmation_rate 
-from
-    signups as s
-left join
-(
-    select 
-        t1.user_id, t1.total, t2.confirmedCnt 
-    from (
-        select 
-            user_id, count(user_id) as total
-        from 
-            confirmations
-        group by
-            user_id
-        ) as t1
-    inner join
-    (
-        select 
-            user_id, count(action) as confirmedCnt
-        from 
-            confirmations
-        group by 
-            user_id, action
-        having 
-            action = 'confirmed'
-    ) as t2
-    on 
-        t1.user_id = t2.user_id
-) as t3
+    Signups.user_id,
+    Round(Avg(If(Confirmations.action='confirmed',1,0)),2) as confirmation_rate 
+from 
+    signups left join Confirmations 
 on 
-    s.user_id = t3.user_id
-
- 
+    Signups.user_id = Confirmations.user_id
+group by 
+    user_id;
