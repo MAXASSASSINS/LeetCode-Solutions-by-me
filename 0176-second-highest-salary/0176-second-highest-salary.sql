4f97f89af9a0c35e11b838948c05ac5156c3f7e4
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2024-06-10 12:10:00 +0530

    Time: 508 ms (50.49%), Space: 0B (100.00%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,15 +1,10 @@
-# Write your MySQL query statement below
 select
-    case 
-        when t2.minsal = t2.maxsal then null
-        else
-            t2.minsal
-    end as SecondHighestSalary
-from
-(    select min(t1.salary) as minsal, max(t1.salary) as maxsal
-    from
-        (select distinct salary
-        from
-            employee
-        order by salary desc
-        limit 2) as t1) as t2
+    (select 
+        distinct salary
+    from 
+        employee
+    order by   
+        salary desc
+    limit 1
+    offset 1)
+    as SecondHighestSalary;
