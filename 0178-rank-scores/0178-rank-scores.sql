d562e66b29cfab8359282cb1dfe65171fc102f73
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2024-06-13 11:27:21 +0530

    Time: 311 ms (93.73%), Space: 0B (100.00%) - LeetHub

---
+++ 0178-rank-scores/0178-rank-scores.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+select s.score as score, t.rank as 'rank'
+from    
+    scores as s,
+    (        
+        select s2.score, sum(if(s2.score < s3.score,1,0)) + 1 as `rank`
+        from
+            (select distinct score from scores) as s2,
+            (select distinct score from scores) as s3
+        group by s2.score
+        order by s2.score desc
+    ) as t
+where
+    s.score = t.score
+order by
+    s.score desc
