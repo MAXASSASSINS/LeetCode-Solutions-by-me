787e02aea99278a7d52b61e88ccf592fa5048169
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2024-06-15 13:30:20 +0530

    Time: 144 ms (54.68%), Space: 85.6 MB (42.83%) - LeetHub

--- 0502-ipo/0502-ipo.cpp
+++ 0502-ipo/0502-ipo.cpp
@@ -1,24 +1,37 @@
 class Solution {
 public:
     int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
-        int n = profits.size();
-        vector<pair<int, int>> projects;
-        for (int i = 0; i < n; i++) {
-            projects.emplace_back(capital[i], profits[i]);
+        int n = capital.size();
+        
+        vector<pair<int,int>> vec;
+        for(int i = 0; i < n; i++){
+            vec.push_back({capital[i], profits[i]});
         }
-        sort(projects.begin(), projects.end());
-        priority_queue<int> q;
-        int ptr = 0;
-        for (int i = 0; i < k; i++) {
-            while (ptr < n && projects[ptr].first <= w) {
-                q.push(projects[ptr++].second);
-            }
-            if (q.empty()) {
-                break;
-            }
-            w += q.top();
-            q.pop();
+        
+        sort(vec.begin(), vec.end());
+        
+        priority_queue<int> pq;
+        
+        int i = 0;
+        
+        while(i < n && w >= vec[i].first){
+            pq.push(vec[i].second);
+            i++;
         }
+        
+        
+        
+        while(k > 0 && !pq.empty()){
+            w += pq.top();
+            pq.pop();
+            
+            while(i < n && w >= vec[i].first){
+                pq.push(vec[i].second);
+                i++;
+            }
+            k--;
+        }
+        
         return w;
     }
 };
