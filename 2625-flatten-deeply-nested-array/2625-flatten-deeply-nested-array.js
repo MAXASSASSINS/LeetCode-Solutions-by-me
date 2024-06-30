4f3aa97219358e6ca554bb14587e4e76e88b109b
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2024-06-30 13:39:37 +0530

    Time: 162 ms (36.85%), Space: 78.6 MB (36.88%) - LeetHub

---
+++ 2625-flatten-deeply-nested-array/2625-flatten-deeply-nested-array.js
@@ -0,0 +1,12 @@
+var flat = function (arr, n) {
+    if(n==0) return arr;
+    var ans = [];
+    for(var i=0; i<arr.length; i++){
+        if(Array.isArray(arr[i])){
+            ans.push(...flat(arr[i], n-1));
+        } else {
+            ans.push(arr[i]);
+        }
+    }
+    return ans;
+};
