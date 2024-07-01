4d7fc111a0042a12e5ce3933e341a196bc25b236
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2024-07-01 11:47:19 +0530

    Time: 119 ms (74.68%), Space: 70.8 MB (69.84%) - LeetHub

---
+++ 2631-group-by/2631-group-by.js
@@ -0,0 +1,21 @@
+/**
+ * @param {Function} fn
+ * @return {Object}
+ */
+Array.prototype.groupBy = function(fn) {
+    const obj = {};
+    
+    this.forEach(x => {
+        let key = fn(x);
+        if(obj[key])
+            obj[key].push(x);
+        else
+            obj[key] = [x];
+    })
+    
+    return obj;
+};
+
+/**
+ * [1,2,3].groupBy(String) // {"1":[1],"2":[2],"3":[3]}
+ */
