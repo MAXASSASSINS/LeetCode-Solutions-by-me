114328711e2dae703083a1aa0971d703ac5db929
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2024-07-04 21:44:47 +0530

    Time: 89 ms (10.56%), Space: 70.1 MB (92.54%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int ans = INT_MIN;
         int n = nums.size();
-        
-        int maxi = *max_element(nums.begin(), nums.end());
-        
+        int ans = INT_MIN;
+        bool allNegatives = true;
+        for(auto x: nums){
+            if(x > 0) allNegatives = false;
+            ans = max(ans, x);
+        }        
+        if(allNegatives) return ans;
+
         int sum = 0;
         for(int i = 0; i < n; i++){
             sum += nums[i];
-            if(sum < 0){
-                sum = 0;
-            }
+            if(sum < 0) sum = 0;
             ans = max(ans, sum);
-        }
-        
-        return ans == 0 ? maxi : ans;
+        }   
+        return ans;
     }
 };
