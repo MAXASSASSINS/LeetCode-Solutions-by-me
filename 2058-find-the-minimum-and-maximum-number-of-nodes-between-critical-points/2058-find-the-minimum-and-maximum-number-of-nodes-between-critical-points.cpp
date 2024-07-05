db141bfa6b04e40f234328f132c6a64f4235fe73
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2024-07-05 14:31:36 +0530

    Time: 516 ms (5.06%), Space: 115.8 MB (79.68%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -1,24 +1,61 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     vector<int> nodesBetweenCriticalPoints(ListNode* head) {
-        ListNode *prev=head;
-        head=head->next;
-        int i=1;
-        vector<int> index;
-        while(head->next){
-            if((prev->val < head->val and head->val > head->next->val) ||( prev->val > head->val and head->val < head->next->val)){
-                index.push_back(i);
-            }
-            prev=head;
-            head=head->next;
-            i++;
-        }
-        if(index.size() < 2) return {-1,-1};
+        vector<int> ans = {-1, -1};
+        if(!head->next->next) return {-1, -1};
         
-        int mindist=INT_MAX;
-        for(int i=0;i<index.size()-1;i++){
-            mindist=min(index[i+1]-index[i],mindist);
+        
+        ListNode *temp = head->next;
+        ListNode *prev = head;
+        
+        int cpIndex = -1;
+        int minCpIndex = -1;
+        
+        int index = 1;
+        
+        while(temp && temp->next){
+            ListNode *next = temp->next;
+            
+            if((next->val < temp->val && prev->val < temp->val) || (next->val > temp->val && prev->val > temp->val)){
+                cout<<prev->val<<temp->val<<next->val<<endl;
+                if(cpIndex == -1){
+                    minCpIndex = index;
+                    cpIndex = index; 
+                }
+                else{
+                    cout<<cpIndex<<index<<minCpIndex<<endl;
+                    int dis = index - cpIndex;
+                    int maxDis = index - minCpIndex;
+                    cpIndex = index;
+                    
+                    if(ans[0] == -1){
+                        ans[0] = dis;
+                        ans[1] = dis;
+                    }
+                    else{
+                        ans[0] = min(ans[0], dis);
+                        ans[1] = maxDis;
+                    }
+                }
+                
+            }
+            index++;
+            prev = temp;
+            temp = temp->next;
         }
-        return {mindist,index.back()-index[0]};
+        
+        return ans;
+        
+        
     }
 };
