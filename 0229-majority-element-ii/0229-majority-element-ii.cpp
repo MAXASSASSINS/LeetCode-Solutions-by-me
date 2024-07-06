829f2d6b0120156d383767cc213451ef433e3f93
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2024-07-06 18:42:36 +0530

    Time: 11 ms (41.55%), Space: 18.2 MB (91.62%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,50 +1,44 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int el1 = INT_MAX;
-        int el2 = INT_MAX;
-        int c1 = 0, c2 = 0;
-        
+        int n = nums.size();
+        int f = INT_MIN, s = INT_MIN;
+        int cnt1 = -1;
+        int cnt2 = -1;
         for(int i = 0; i < nums.size(); i++){
-            if(c1 == 0 && nums[i] != el2){
-                c1 = 1;
-                el1 = nums[i];
+            if(cnt1 == -1 && nums[i] != s){
+                f = nums[i];
+                cnt1++;
             }
-            else if(c2 == 0 && nums[i] != el1){
-                c2 = 1;
-                el2 = nums[i];
+            else if(cnt2 == -1 && nums[i] != f){
+                s = nums[i];
+                cnt2++;
             }
-            else if(nums[i] == el1){
-                c1++;
+            else if(f == nums[i]){
+                cnt1++;
             }
-            else if(nums[i] == el2){
-                c2++;
+            else if(s == nums[i]){
+                cnt2++;
             }
             else{
-                c1--;
-                c2--;
+                cnt2--;
+                cnt1--;
             }
         }
-        
-        c1 = 0, c2 = 0;
-        for(int i = 0; i < nums.size(); i++){
-            if(nums[i] == el1){
-                c1++;
-            }
-            else if(nums[i] == el2){
-                c2++;
-            }
-        }
-        
         vector<int> ans;
-        if(c1 > nums.size() / 3){
-            ans.push_back(el1);
+        cnt1 = 0, cnt2 = 0;
+        for(int i = 0; i < nums.size(); i++){
+            if(f == nums[i]) cnt1++;
+            if(s == nums[i]) cnt2++;
         }
-        if(c2 > nums.size() / 3){
-            ans.push_back(el2);
+        if(cnt1 > n /3){
+            ans.push_back(f);
         }
-        
+        if(cnt2 > n / 3){
+            ans.push_back(s);
+        }
         return ans;
     }
+    
     
 };
