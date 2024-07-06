a68b0ec5de5a7f0e6266ef800bdfb2c07b4c640f
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2024-07-06 10:39:53 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (30.58%) - LeetHub

---
+++ 2582-pass-the-pillow/2582-pass-the-pillow.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int passThePillow(int n, int time) {
+        time %= ((n - 1) * 2);
+        if(time < n){
+            return time + 1;
+        }
+        return 2*n - 1 - time;
+    }
+};
