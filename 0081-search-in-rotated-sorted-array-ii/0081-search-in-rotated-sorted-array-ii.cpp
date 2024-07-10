7c83caf0a719fb3626b971409f5c3e9a6d2e3860
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2024-07-10 13:41:21 +0530

    Time: 10 ms (12.21%), Space: 16.6 MB (6.52%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
-    bool bs(vector<int> &nums, int low, int high, int target){
+    bool search(vector<int>& nums, int target) {
+        int low = 0;
+        int high = nums.size() -1;
+        
         while(low <= high){
-            int mid = low + (high - low) / 2;
-            if(target == nums[mid]) return true;
-            else if(target < nums[mid]){
-                high = mid - 1;
+            int mid = low  + (high - low) / 2;
+            int x = nums[mid];
+            
+            if(x == target) return true;
+            else if(nums[low] == nums[mid] && nums[high] == nums[mid]){
+                high--;
+                low++;
+            }
+            else if(nums[low] <= nums[mid]){
+                if(nums[low] <= target && target <= nums[mid]){
+                    high = mid - 1;
+                }
+                else{
+                    low = mid + 1;
+                }
             }
             else{
-                low = mid + 1;
+                if(nums[mid] <= target && target <= nums[high]){
+                    low = mid + 1;
+                }
+                else{
+                    high = mid - 1;
+                }
             }
         }
-        
         return false;
-    }
-    bool search(vector<int>& nums, int target) {
-        int n = nums.size();
-        int pivot = -1;
-        for(int i = 0; i < n; i++){
-            if(i + 1 < n && nums[i] > nums[i + 1]){
-                pivot = i + 1;
-            }
-        }
-        
-        if(pivot == -1){
-            return bs(nums, 0, n - 1, target);
-        }
-        
-        bool left = bs(nums, 0, pivot - 1, target);
-        bool right = bs(nums, pivot, n - 1, target);
-        
-        return left || right;
     }
 };
