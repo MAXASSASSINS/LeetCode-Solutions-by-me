34f11354f15c4182393426851766c1891dc460d2
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2024-07-11 00:18:17 +0530

    Time: 19 ms (54.17%), Space: 24.6 MB (85.94%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,27 +1,36 @@
 class Solution {
 public:
-    int binary(vector<int> &nums, int st, int en, int n){
-        int mid = st + (en - st)/2;
-        if(mid % 2 == 1 && mid + 1 < n  && nums[mid] == nums[mid + 1]){
-            return binary(nums, st, mid - 1, n);
-        }
-        if(mid % 2 == 1 && mid - 1 >= 0 && nums[mid] == nums[mid - 1]){
-            return binary(nums, mid + 1, en, n);
-        }
-        if(mid % 2 == 0 && mid + 1 < n && nums[mid] == nums[mid + 1]){
-            return binary(nums, mid + 1, en, n);
-        }
-        if(mid % 2 == 0 && mid - 1 >= 0 && nums[mid] == nums[mid - 1]){
-            return binary(nums, st, mid - 1, n);
-        }
-
-        return nums[mid];
-    }
     int singleNonDuplicate(vector<int>& nums) {
         int n = nums.size();
-        if(n == 1){
-            return nums[0];
+        if(nums.size() == 1) return nums[0];
+        if(nums[0] != nums[1]) return nums[0];
+        if(nums[n - 1] != nums[n - 2]) return nums[n - 1];
+        
+        int low = 1;
+        int high = nums.size() - 2;
+        
+        while(low <= high){
+            int mid = (low + high) / 2;
+            cout<<mid<<endl;
+            if(nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]) return nums[mid];
+            if(mid % 2 == 0){
+                if(nums[mid] == nums[mid - 1]){
+                    high = mid - 1;
+                }
+                else{
+                    low = mid + 1;
+                }
+            }   
+            else{
+                if(nums[mid] == nums[mid - 1]){
+                    low = mid + 1;
+                }
+                else{
+                    high = mid - 1;
+                }
+            }
         }
-        return binary(nums, 0, n, n);
+        
+        return -1;
     }
 };
