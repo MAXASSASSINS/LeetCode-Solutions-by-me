d3f7060df71eafb3d9b6945b0d76b1f886fd0900
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2024-07-20 20:04:07 +0530

    Time: 3 ms (47.60%), Space: 7.7 MB (34.08%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -2,12 +2,26 @@ class Solution {
 public:
     bool rotateString(string s, string goal) {
         if(s.length() != goal.length()) return false;
-        goal += goal;
-        
-        auto it = goal.find(s);
-        if(it != string::npos){
-            return true;
+        string t = s + s;
+        // cout<<t<<endl;
+        int i = 0;
+        while(i < t.length()){
+            if(t[i] == goal[0]){
+                // cout<<i<<endl;
+                int j = 0;
+                while(i < t.length() && j < goal.length() && t[i] == goal[j]){
+                    i++;
+                    j++;
+                }
+                cout<<j<<endl;
+                if(j >= goal.length()){
+                    return true;
+                }
+                i -= j;
+            }
+            i++;
         }
+        
         return false;
     }
 };
