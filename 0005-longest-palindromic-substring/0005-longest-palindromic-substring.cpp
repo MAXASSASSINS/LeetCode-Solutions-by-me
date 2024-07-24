b66ce2a2497077f74aec7646b4a69f6d89723f6c
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2024-07-24 18:52:44 +0530

    Time: 13 ms (78.16%), Space: 11.1 MB (67.12%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,32 +1,34 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int ans = 0;
-        string res;
-        for(int i = 0; i < s.length(); i++){
-            string odd = expand(s, i, i);
-            string even = expand(s, i, i + 1);
-            getMaxLengthPalindromicSubstring(odd, ans, res);
-            getMaxLengthPalindromicSubstring(even, ans, res);
+        string ans;
+        int n = s.length();
+        for(int i = 0; i < n; i++){
+            int j = i - 1;
+            int k = i + 1;
+            while(j >= 0 && k < n && s[j] == s[k]){
+                j--;
+                k++;
+            }
+            if(ans.length() < k - j - 1){
+                ans = s.substr(j + 1, k - j - 1);
+            }
         }
         
-        return res;
-    }
-    
-    string expand(string s, int l, int r){
-        while(l >= 0 && r < s.length()){
-            if(s[l] != s[r]) break;
-            l--;
-            r++;
+        for(int i = 0; i < n - 1; i++){
+            if(s[i] == s[i + 1]){
+                int j = i - 1;
+                int k = i + 2;
+                while(j >= 0 && k < n && s[j] == s[k]){
+                    j--;
+                    k++;
+                }
+                if(ans.length() < k - j - 1){
+                    ans = s.substr(j + 1, k - j - 1);
+                }
+            }
         }
         
-        return s.substr(l + 1, r - l - 1);
-    }
-    
-    void getMaxLengthPalindromicSubstring(string s, int &ans, string &res){
-        if(s.length() > ans){
-            ans = s.length();
-            res = s;
-        }
+        return ans;
     }
 };
