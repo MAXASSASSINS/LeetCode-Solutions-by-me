2d426971d88866e61d046049ab12326713219731
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2024-07-24 19:06:56 +0530

    Time: 94 ms (87.40%), Space: 19.6 MB (59.84%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -1,20 +1,34 @@
 class Solution {
 public:
     int beautySum(string s) {
-        int res = 0;
-        for (auto i = 0; i < s.size(); ++i) {
-            int cnt[26] = {}, max_f = 0, min_f = 0;
-            for (auto j = i; j < s.size(); ++j) {
-                int idx = s[j] - 'a';
-                max_f = max(max_f, ++cnt[idx]);
-                if (min_f >= cnt[idx] - 1) {
-                    min_f = cnt[idx];
-                    for (int k = 0; k < 26; ++k)
-                        min_f = min(min_f, cnt[k] == 0 ? INT_MAX : cnt[k]);
+        int ans = 0;
+        int n = s.length();
+        
+        for(int i = 0; i < n; i++){
+            vector<int> counts(26, 0);
+            string temp = "";
+            
+            for(int j = i; j < n; j++){
+                char c = s[j];
+                temp += c;
+                counts[c - 'a']++;
+                
+                int maxi = INT_MIN, mini = INT_MAX;
+                for(auto x: counts){
+                    maxi = max(maxi, x);
+                    if(x != 0){
+                        mini = min(mini, x);
+                    }
                 }
-                res += max_f - min_f;
+                
+                if(mini != INT_MIN){
+                    ans += maxi - mini;
+                }
+                
             }
+            
         }
-    return res;
-}
+        
+        return ans;
+    }
 };
