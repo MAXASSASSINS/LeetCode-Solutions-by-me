00ed0ce7a749b2e12f2ba538153a15c41ce54941
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2024-07-28 20:55:50 +0530

    Time: 62 ms (16.74%), Space: 16.7 MB (9.20%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        // O(N) time , O(1) space [because hasmap size is always 3]
-        int left = 0 , right = 0 , end = s.size() - 1;
-        unordered_map<char,int> map;
-        
+        int n = s.length();
         int count = 0;
-        
-        while(right != s.size())
-        {
-            map[s[right]] += 1; // adding new element to window
-            
-            while(map['a'] and map['b'] and map['c']) // while(window is good)
-            {
-                count += 1 + (end - right);
-                
-				// removing first element[means shrinking window from front to form new window]
-                map[s[left]] -= 1; 
-                left++;
+        int j = 0;
+        unordered_map<int,int> m;
+        for(int i = 0; i < n; i++){
+            m[s[i]]++;
+            while(m.size() == 3){
+                count += n - i;
+                m[s[j]]--;
+                if(m[s[j]] == 0){
+                    m.erase(s[j]);
+                }
+                j++;
             }
-            right++;
         }
+        
         return count;
     }
 };
