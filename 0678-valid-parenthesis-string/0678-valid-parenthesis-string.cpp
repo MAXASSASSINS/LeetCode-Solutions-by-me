092ca72025a7301d62a314669b553bb447fe0113
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2024-08-04 13:59:49 +0530

    Time: 0 ms (100.00%), Space: 12.5 MB (7.54%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
-    bool checkValidString(string s) {
-        int leftMin = 0, leftMax = 0;
+    bool solve(string &s, int index, int cnt, vector<vector<int>> &dp){
+        if(cnt < 0) return false;
         
-        for(auto c : s){
-            if(c == '('){
-                leftMin++;
-                leftMax++;
-            }
-            else if(c == ')'){
-                leftMin--;
-                leftMax--;
-            }
-            else{
-                leftMin--;
-                leftMax++;
-            }
-            if(leftMax < 0){
-                return false;
-            }
-            if(leftMin < 0){
-                leftMin = 0;
-            }
+        if(index >= s.length()){
+            return cnt == 0;
         }
         
-        return leftMin == 0;
+        if(dp[index][cnt] != -1) return dp[index][cnt];
+        
+        bool open, close, empty;
+        if(s[index] == '('){
+            return dp[index][cnt] = solve(s, index + 1, cnt + 1, dp);
+        }
+        else if(s[index] == ')'){
+            return dp[index][cnt] = solve(s, index + 1, cnt - 1, dp);
+        }
+        return dp[index][cnt] = 
+            solve(s, index + 1, cnt + 1, dp) ||
+            solve(s, index + 1, cnt - 1, dp) ||
+            solve(s, index + 1, cnt, dp);
+    }
+    
+    bool checkValidString(string s) {
+        vector<vector<int>> dp(101, vector<int>(101, -1));
+        return solve(s, 0, 0, dp);
     }
 };
