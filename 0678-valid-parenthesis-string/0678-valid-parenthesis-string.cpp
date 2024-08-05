f4183b5f861f0597ccd2de21934c2b21a48ee5e4
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2024-08-05 18:41:12 +0530

    Time: 3 ms (51.62%), Space: 7.6 MB (37.76%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
-    bool solve(string &s, int index, int cnt, vector<vector<int>> &dp){
-        if(cnt < 0) return false;
-        
-        if(index >= s.length()){
-            return cnt == 0;
-        }
-        
-        if(dp[index][cnt] != -1) return dp[index][cnt];
-        
-        bool open, close, empty;
-        if(s[index] == '('){
-            return dp[index][cnt] = solve(s, index + 1, cnt + 1, dp);
-        }
-        else if(s[index] == ')'){
-            return dp[index][cnt] = solve(s, index + 1, cnt - 1, dp);
-        }
-        return dp[index][cnt] = 
-            solve(s, index + 1, cnt + 1, dp) ||
-            solve(s, index + 1, cnt - 1, dp) ||
-            solve(s, index + 1, cnt, dp);
-    }
-    
     bool checkValidString(string s) {
-        vector<vector<int>> dp(101, vector<int>(101, -1));
-        return solve(s, 0, 0, dp);
+        int mini = 0; 
+        int maxi = 0;
+        
+        for(int i = 0; i < s.length(); i++){
+            if(s[i] == '('){
+                mini++;
+                maxi++;
+            }
+            else if(s[i] == ')'){
+                mini > 0 ? mini-- : 0;
+                maxi--;
+            }
+            else{
+                mini > 0 ? mini-- : 0;
+                maxi++;
+            }
+            
+            if(maxi < 0) return false;
+        }
+        
+        return mini == 0;
     }
 };
