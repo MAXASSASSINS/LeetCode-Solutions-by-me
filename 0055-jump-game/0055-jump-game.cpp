b3ec36c29f9ffc38f3c4e17320e20b9d096b979d
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2024-08-05 20:45:13 +0530

    Time: 53 ms (25.78%), Space: 51.1 MB (45.44%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -3,15 +3,14 @@ public:
     bool canJump(vector<int>& nums) {
         int n = nums.size();
         
-        int goal = n - 1;
+        int target = n - 1;
         
         for(int i = n - 2; i >= 0; i--){
-            if(nums[i] + i >= goal){
-                goal = i;
-            }
+            if(target - i <= nums[i]){
+                target = i;
+            }    
         }
         
-        return goal == 0;
-        
+        return target == 0;
     }
 };
