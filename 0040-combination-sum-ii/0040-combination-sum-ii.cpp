53b234d7c5f9dde1f692d5f08cb9cb4ff01029fe
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2024-08-13 08:57:30 +0530

    Time: 3 ms (77.81%), Space: 12.8 MB (65.98%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    void solve(vector<int> &candidates, int target, int index, vector<vector<int>> &ans, vector<int> &temp){
-        if(target == 0){
-            ans.push_back(temp);
+    vector<vector<int>> combinationSum2(vector<int> &candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        vector<vector<int>> result;
+        vector<int> combination;
+        combination2(result, combination, candidates, target, 0);
+        return result;
+    }
+    void combination2(vector<vector<int>> &res, vector<int> &combination,
+                      vector<int> &candidates, int target, int index) {
+        if (target == 0) {
+            res.push_back(combination);
             return;
         }
-
-
-
-        for(int i = index; i < candidates.size(); i++){
-            if(i > index && candidates[i] == candidates[i - 1]) continue;
-            if(candidates[i] > target) break;
-            temp.push_back(candidates[i]);
-            solve(candidates, target - candidates[i], i + 1, ans, temp);
-            temp.pop_back();
+        for (int i = index; i < candidates.size() && target >= candidates[i];
+             ++i) {
+            if (i == index || candidates[i] != candidates[i - 1]) {
+                combination.push_back(candidates[i]);
+                combination2(res, combination, candidates,
+                             target - candidates[i], i + 1);
+                combination.pop_back();
+            }
         }
-    }
-    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        sort(candidates.begin(), candidates.end());
-        vector<vector<int>> ans;
-        vector<int> temp;
-        solve(candidates, target, 0, ans, temp);
-        return ans;
     }
 };
