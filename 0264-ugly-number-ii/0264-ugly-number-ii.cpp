f187ba7b6c9aee7cc49c63a1e515007cae18fcc2
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2024-08-18 13:37:39 +0530

    Time: 73 ms (30.38%), Space: 31.9 MB (22.25%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.cpp
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
     int nthUglyNumber(int n) {
-        vector<int> prime = {2,3,5};
-        vector<int> temp = {1};
-        int c2 = 0;
-        int c3 = 0;
-        int c5 = 0;
-        while(temp.size() <= n){
-            int num = (min(2 * temp[c2], min(3 * temp[c3], 5 * temp[c5])));
-            if(num == 2 * temp[c2])
-                c2++;
-            if(num == 3 * temp[c3])
-                c3++;
-            if(num == 5 * temp[c5])
-                c5++;
-            temp.push_back(num);
+        set<long long> st;
+        st.insert(1);
+        int currentUgly = 1;
+        for(int i = 0; i < n; i++){
+            currentUgly = *st.begin();
+            
+            st.insert(currentUgly * 1LL * 2);
+            st.insert(currentUgly * 1LL * 3);
+            st.insert(currentUgly * 1LL * 5);
+            
+            st.erase(currentUgly);
         }
         
-        return temp[n - 1];
-        
+        return currentUgly;
     }
 };
