0396a79546ed15059fa39d8e8f606123aad215d0
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2024-08-19 18:05:54 +0530

    Time: 28 ms (16.71%), Space: 159 MB (8.56%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
-    int solve(int n, int count, int copied){
+    int solve(int n, int count, int copied, vector<vector<int>> &dp){
         if(count == n) return 0;
         if(count > n) return 1e9; 
         
-        int pasteNew = 2 + solve(n, count + count, count);
+        if(dp[count + 2][copied + 2] != -1) return dp[count + 2][copied + 2]; 
+        
+        int pasteNew = 2 + solve(n, count + count, count, dp);
         int pasteOld = INT_MAX;
         if(copied != -1){
-            pasteOld = 1 + solve(n, count + copied, copied);
+            pasteOld = 1 + solve(n, count + copied, copied, dp);
         }
         
-        return min(pasteOld, pasteNew);
+        return dp[count + 2][copied + 2] = min(pasteOld, pasteNew);
     }
     int minSteps(int n) {
         if(n == 1) return 0;
-        return solve(n, 1, -1);
+        vector<vector<int>> dp(n + 2, vector<int>(n + 2, -1));
+        return solve(n, 1, -1, dp);
         // return 0;
     }
 }; 
