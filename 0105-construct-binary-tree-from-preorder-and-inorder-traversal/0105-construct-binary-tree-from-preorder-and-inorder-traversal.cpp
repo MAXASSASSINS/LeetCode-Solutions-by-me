0f6d1edf132a6a26456223bd1e506662cc515fec
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2024-08-24 15:24:46 +0530

    Time: 11 ms (70.85%), Space: 27.4 MB (47.61%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -11,32 +11,28 @@
  */
 class Solution {
 public:
-    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        unordered_map<int, int> m;
+    TreeNode *solve(vector<int> &preorder, vector<int> &inorder, int l1, int r1, int l2, int r2, unordered_map<int,int> &m){
+        if(l1 > r1 || l2 > r2) return nullptr;
         
-        for(int i = 0; i < inorder.size(); i++){
+        TreeNode *root = new TreeNode(preorder[l1]);
+        
+        int index = m[preorder[l1]];
+        
+        int numsLeft = index - l2;
+        
+        root->left = solve(preorder, inorder, l1 + 1, l1 + numsLeft, l2, index - 1, m);
+        root->right = solve(preorder, inorder, l1 + numsLeft + 1, r1 ,index + 1, r2, m);    
+        return root;
+        
+    }
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {  
+        int n = inorder.size();
+        
+        unordered_map<int,int> m;
+        for(int i = 0; i < n; i++){
             m[inorder[i]] = i;
         }
         
-        TreeNode *ans = buildTree(preorder, 0, preorder.size() - 1, inorder, 0, inorder.size() - 1, m);
-        
-        return ans;
+        return solve(preorder, inorder, 0, n - 1, 0, n - 1, m);
     }
-    
-    TreeNode* buildTree(vector<int>& preorder, int preStart, int preEnd, vector<int>& inorder, int inStart, int inEnd, unordered_map<int, int> &m){
-        
-        if(preStart > preEnd || inStart > inEnd) return nullptr;
-        
-        TreeNode *root = new TreeNode(preorder[preStart]);
-        
-        int inRoot = m[root->val];
-        int numsLeft = inRoot - inStart;
-        
-        root->left = buildTree(preorder, preStart + 1, preStart + numsLeft, inorder, inStart, inRoot - 1, m);
-        
-         root->right = buildTree(preorder, preStart + 1 + numsLeft, preEnd, inorder, inRoot + 1, inEnd, m);
-        
-        return root;
-    }
-    
 };
