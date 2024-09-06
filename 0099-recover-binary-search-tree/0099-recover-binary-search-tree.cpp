ac8cb579971e5857a89eec6fbdc33d03588140e9
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2024-09-06 17:36:38 +0530

    Time: 20 ms (66.81%), Space: 60.8 MB (55.62%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
@@ -1,24 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
+private:
+    TreeNode *prev, *f, *m, *l;
 public:
-	TreeNode* firstMistake, *secondMistake, *pre;
-	void recoverTree(TreeNode* root) {
-		pre = new TreeNode(INT_MIN);
-		inorder(root);
-		swap(firstMistake->val, secondMistake->val);
-	}
-
-	void inorder(TreeNode* root) {
-		if(root == nullptr) 
-			return;
-
-		inorder(root->left);
-
-		if(firstMistake == nullptr && root->val < pre->val)
-			firstMistake = pre;
-		if(firstMistake != nullptr && root->val < pre->val)
-			secondMistake = root;
-		pre = root;
-
-		inorder(root->right);
-	}
+    void solve(TreeNode *root){
+        if(!root) return;
+        
+        solve(root->left);
+        
+        if(prev->val > root->val){
+            if(!f){
+                f = prev;
+                m = root;
+            }
+            else{
+                l = root;
+            }
+        }
+        prev = root;
+        solve(root->right);
+    }
+    void recoverTree(TreeNode* root) {
+        prev = new TreeNode(INT_MIN);
+        f = m = l = NULL;
+        solve(root);
+        if(f && l){
+            swap(f->val, l->val);
+        }
+        else{
+            swap(f->val, m->val);
+        }
+    }
 };
