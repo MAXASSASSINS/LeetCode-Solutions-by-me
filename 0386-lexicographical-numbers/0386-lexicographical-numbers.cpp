b140a8f997aee764059a7a2d4b7f9e6a4839968c
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2024-09-21 11:36:53 +0530

    Time: 0 ms (100.00%), Space: 12.4 MB (91.06%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -1,14 +1,25 @@
 class Solution {
 public:
     vector<int> lexicalOrder(int n) {
-        vector<string> res;
-        for(int i = 1; i <= n; i++){
-            res.push_back(to_string(i));
-        }
-        sort(res.begin(), res.end());
         vector<int> ans;
-        for(auto s: res){
-            ans.push_back(stoi(s));
+        int st = 1;
+        while(ans.size() < n){
+            while(st * 10 <= n){
+                ans.push_back(st);
+                st *= 10;
+            }
+            if(st <= n)
+                ans.push_back(st);
+            int i = st + 1;
+            while(i % 10 != 0 && i <= n){
+                ans.push_back(i);
+                i++;
+            }
+            int en = i; 
+            while(en % 10 == 0){
+                en /= 10;
+            }
+            st = en;
         }
         return ans;
     }
