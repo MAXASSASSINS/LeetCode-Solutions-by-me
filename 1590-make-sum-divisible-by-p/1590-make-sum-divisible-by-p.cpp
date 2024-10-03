3a9cd4f652f678bcbabd373a6c46a061abb2aea7
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2024-10-03 22:07:14 +0530

    Time: 127 ms (84.32%), Space: 70.3 MB (94.34%) - LeetHub

--- 1590-make-sum-divisible-by-p/1590-make-sum-divisible-by-p.cpp
+++ 1590-make-sum-divisible-by-p/1590-make-sum-divisible-by-p.cpp
@@ -1,20 +1,41 @@
 class Solution {
 public:
     int minSubarray(vector<int>& nums, int p) {
-        int tmp = 0, res = nums.size(), need_to_remove = 0;
-        for (auto num : nums) need_to_remove = (need_to_remove + num) % p;
-        if (need_to_remove == 0) return 0;
-        
-        unordered_map<int, int> pre_sum;
-        pre_sum[0] = -1;
-        
-        for (int i = 0; i < nums.size(); i++) {
-            tmp = (tmp + nums[i]) % p;
-            pre_sum[tmp] = i;
-            int rest = (tmp - need_to_remove + p) % p;
-            
-            if (pre_sum.find(rest) != pre_sum.end()) res = min(res, i - pre_sum[rest]);
+        int n = nums.size();
+        int totalSum = 0;
+
+        // Step 1: Calculate total sum and target remainder
+        for (int num : nums) {
+            totalSum = (totalSum + num) % p;
         }
-        return res >= nums.size() ? -1 : res;
+
+        int target = totalSum % p;
+        if (target == 0) return 0;  // The array is already divisible by p
+
+        // Step 2: Use a hash map to track prefix sum mod p
+        unordered_map<int, int> modMap;
+        modMap[0] =
+            -1;  // To handle the case where the whole prefix is the answer
+        int currentSum = 0, minLen = n;
+
+        // Step 3: Iterate over the array
+        for (int i = 0; i < n; ++i) {
+            currentSum = (currentSum + nums[i]) % p;
+
+            // Calculate what we need to remove
+            int needed = (currentSum - target + p) % p;
+
+            // If we have seen the needed remainder, we can consider this
+            // subarray
+            if (modMap.find(needed) != modMap.end()) {
+                minLen = min(minLen, i - modMap[needed]);
+            }
+
+            // Store the current remainder and index
+            modMap[currentSum] = i;
+        }
+
+        // Step 4: Return result
+        return minLen == n ? -1 : minLen;
     }
 };
