725a6c1cf863c71721e1e667a4dc2367d2b22aad
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2024-11-11 21:28:03 +0530

    Time: 15 ms (20.21%), Space: 28.9 MB (31.95%) - LeetHub

---
+++ 2601-prime-subtraction-operation/2601-prime-subtraction-operation.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<int> fillPrimes(int maxi){
+        vector<bool> vec(maxi + 1, true);
+        vec[0] = false;
+        vec[1] = false;
+        
+        for(int i = 2; i < vec.size(); i++){
+            if(vec[i]){
+                for(int j = 2 *i ; j < vec.size(); j+= i){
+                    vec[j] = false;
+                }
+            }
+        }
+        
+        vector<int> primes;
+        for(int i = 0; i < vec.size(); i++){
+            if(vec[i]) primes.push_back(i);
+        }
+        
+        return primes;
+    }
+    
+    bool primeSubOperation(vector<int>& nums) {
+        vector<int> primes = fillPrimes(1000);
+        cout<<primes.size()<<endl;
+        int n = nums.size();
+        
+        for(int i = n - 2; i >= 0; i--){
+            if(nums[i] >= nums[i + 1]){
+                int index = lower_bound(primes.begin(), primes.end(), nums[i] - nums[i + 1] + 1) - primes.begin();
+                // cout<<index<<"\t";
+                if(primes[index] >= nums[i]) return false;
+                nums[i] -= primes[index];
+                if(nums[i] > nums[i + 1]) return false;
+            }
+        }
+        
+        for(auto x: nums){
+            // cout<<x<<"\t";
+        }
+        
+        return true;
+    }
+};
