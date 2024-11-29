fb667bd99c6a6e46639ee16471076fefda7f2bee
Mohd Shadab <97503316+MAXASSASSINS@users.noreply.github.com>
2024-11-29 20:32:34 +0530

    Time: 187 ms (14.50%), Space: 53.6 MB (8.44%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,43 +1,35 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        vector<vector<int>> ans;
-        sort(nums.begin(), nums.end());
+        set<vector<int>> res;
         int n = nums.size();
-        for (int i = 0; i < n; ++i) {
-            for (int j = i + 1; j < n; ++j) {
-                long long int newTarget = long ((long)target - (long)nums[i] - (long)nums[j]);
-                int left = j + 1;
-                int right = n - 1;
-
-                while (left < right){
-                    long long int twoSum = nums[left] + nums[right];
-                    if (twoSum < newTarget){
-                        left++;
+        sort(nums.begin(), nums.end());
+        for(int i = 0; i < n; i++){
+            for(int j = i + 1; j < n; j++){
+                long long reqSum = 1LL * target - (nums[i] + nums[j]);
+                
+                int k = j + 1;
+                int l = n - 1;
+                while(k < l){
+                    long long sum = nums[k] + nums[l];
+                    
+                    if(sum > reqSum){
+                        l--;
                     }
-                    else if(twoSum > newTarget){
-                        right--;
+                    else if(sum < reqSum){
+                        k++;
                     }
                     else{
-                        vector<int> quad(4, 0);
-                        quad[0] = nums[i];
-                        quad[1] = nums[j];
-                        quad[2] = nums[left];
-                        quad[3] = nums[right];
-                        ans.push_back(quad);
-
-                        while (left < right && nums[left] == quad[2])
-                            left++;
-                        while (left < right && nums[right] == quad[3])
-                            right--;
+                        vector<int> temp = {nums[i], nums[j], nums[k], nums[l]};
+                        res.insert(temp);
+                        l--;
+                        k++;
                     }
                 }
-                while (j + 1 < n && nums[j] == nums[j + 1])
-                    j++;
             }
-            while (i + 1 < n && nums[i] == nums[i + 1])
-                i++;
         }
+        
+        vector<vector<int>> ans(res.begin(), res.end());
         return ans;
     }
 };
